3eee8e204066bda4397f319263408fef0bfab31d
Sergeyev Ilya <ilya.a.sergeyev@gmail.com>
2014-07-18 16:40:47 +0300

    control of second v4l2 instance creation
    	modified:   modules/v4l2/v4l2.c

--- modules/v4l2/v4l2.c
+++ modules/v4l2/v4l2.c
@@ -610,7 +610,7 @@ static int alloc(struct vidsrc_st **stp, struct vidsrc *vs,
 		error("v4l2 busy - try to close device.\n");
 		vst = mem_deref(vst);
 		if (!vst)
-			error("v4l2: device closed.\n");
+			info("v4l2: device closed.\n");
 		else
 			error("v4l2: can't close device.\n");
 	}
