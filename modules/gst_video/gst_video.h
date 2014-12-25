f55560474869956b374606973e4633206e9bf0da
Alfred E. Heggestad <aeh@db.org>
2014-12-25 17:36:37 +0100

    gst_video: new vidcodec module using gstreamer
    the original code was based on avcodec, and written by
    Victor Sergienko and Fadeev Alexander. Later the code was
    reviewed by Alfred and cleaned up a bit.
    
    this module can use hardware-encoding of H.264 for
    devices that support it (via gstreamer), by tweaking
    the pipeline string a bit.

---
+++ modules/gst_video/gst_video.h
@@ -0,0 +1,32 @@
+/**
+ * @file gst_video.h  Gstreamer video pipeline -- internal API
+ *
+ * Copyright (C) 2010 - 2014 Creytiv.com
+ * Copyright (C) 2014 Fadeev Alexander
+ */
+
+
+/* Encode */
+struct videnc_state;
+
+int gst_video_encode_update(struct videnc_state **vesp,
+			    const struct vidcodec *vc,
+			    struct videnc_param *prm, const char *fmtp);
+int gst_video_encode(struct videnc_state *st, bool update,
+		     const struct vidframe *frame,
+		     videnc_packet_h *pkth, void *arg);
+
+
+/* SDP */
+uint32_t gst_video_h264_packetization_mode(const char *fmtp);
+int      gst_video_fmtp_enc(struct mbuf *mb, const struct sdp_format *fmt,
+			    bool offer, void *arg);
+bool     gst_video_fmtp_cmp(const char *fmtp1, const char *fmtp2, void *data);
+
+
+/* H.264 */
+extern const uint8_t h264_level_idc;
+
+int h264_packetize(const uint8_t *buf, size_t len,
+		   size_t pktsize,
+		   videnc_packet_h *pkth, void *arg);
---
+++ modules/gst_video/module.mk
@@ -0,0 +1,12 @@
+#
+# module.mk
+#
+# Copyright (C) 2010 Creytiv.com
+#
+
+MOD		:= gst_video
+$(MOD)_SRCS	+= gst_video.c h264.c encode.c sdp.c
+$(MOD)_LFLAGS	+= `pkg-config --libs gstreamer-0.10 gstreamer-app-0.10`
+CFLAGS          += `pkg-config --cflags gstreamer-0.10 gstreamer-app-0.10`
+
+include mk/mod.mk
