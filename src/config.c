25e09c6de764418ab233780a3adb3bef27371dd7
Juha Heinanen <jh@tutpro.com>
2015-06-04 11:52:37 +0300

    src, docs: added opus codec params to default config and example

--- src/config.c
+++ src/config.c
@@ -692,6 +692,9 @@ int config_write_template(const char *file, const struct config *cfg)
 	(void)re_fprintf(f, "speex_vad\t\t0 # Voice Activity Detection 0-1\n");
 	(void)re_fprintf(f, "speex_agc_level\t\t8000\n");
 
+	(void)re_fprintf(f, "\n# Opus codec parameters\n");
+	(void)re_fprintf(f, "opus_bitrate\t\t28000 # 6000-510000\n");
+
 	(void)re_fprintf(f, "\n# NAT Behavior Discovery\n");
 	(void)re_fprintf(f, "natbd_server\t\tcreytiv.com\n");
 	(void)re_fprintf(f, "natbd_interval\t\t600\t\t# in seconds\n");
