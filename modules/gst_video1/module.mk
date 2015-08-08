ebac23b0692de71ee4c3a436f0372013150c937f
Alfred E. Heggestad <aeh@db.org>
2015-08-08 12:21:34 +0200

    gst1/gst_video1: add -Wno-cast-align to CFLAGS to avoid clang warnings

--- modules/gst_video1/module.mk
+++ modules/gst_video1/module.mk
@@ -8,5 +8,6 @@ MOD		:= gst_video1
 $(MOD)_SRCS	+= gst_video.c h264.c encode.c sdp.c
 $(MOD)_LFLAGS	+= `pkg-config --libs gstreamer-1.0 gstreamer-app-1.0`
 $(MOD)_CFLAGS   += `pkg-config --cflags gstreamer-1.0 gstreamer-app-1.0`
+$(MOD)_CFLAGS	+= -Wno-cast-align
 
 include mk/mod.mk
