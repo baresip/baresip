d37323d2abac52ce4d6fc8e421422db0c6bba58e
Alfred E. Heggestad <aeh@db.org>
2016-08-07 11:53:05 +0200

    change value of KEYCODE_REL to 0x04, EOT (end of transmission)

--- include/baresip.h
+++ include/baresip.h
@@ -643,7 +643,7 @@ int  ui_password_prompt(char **passwordp);
 
 /* special keys */
 #define KEYCODE_NONE   (0x00)
-#define KEYCODE_REL    (-1)    /* Key was released */
+#define KEYCODE_REL    (0x04)    /* Key was released */
 #define KEYCODE_ESC    (0x1b)
 
 
