2e31901b6faf0bdef07cb69b76da1daa418318a5
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2017-01-04 21:17:23 +0100

    swscale: add NV21 pixel format

--- modules/swscale/swscale.c
+++ modules/swscale/swscale.c
@@ -26,6 +26,7 @@ static enum AVPixelFormat vidfmt_to_avpixfmt(enum vidfmt fmt)
 
 	case VID_FMT_YUV420P: return AV_PIX_FMT_YUV420P;
 	case VID_FMT_NV12:    return AV_PIX_FMT_NV12;
+	case VID_FMT_NV21:    return AV_PIX_FMT_NV21;
 	default:              return AV_PIX_FMT_NONE;
 	}
 }
