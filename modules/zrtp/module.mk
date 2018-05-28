dcaf873131971c21b93b29209bec10fc1c6a6030
Juha Heinanen <jh@tutpro.com>
2018-05-28 22:24:24 +0300

    added build instructions as comments to zrtp/module.mk

--- modules/zrtp/module.mk
+++ modules/zrtp/module.mk
@@ -4,6 +4,18 @@
 # Copyright (C) 2010 Creytiv.com
 #
 
+#
+# To build zrtp libraries and include files, run the following commands:
+#
+#     git clone git@github.com:juha-h/libzrtp.git
+#     cd libzrtp
+#     ./bootstrap.sh
+#     ./configure CFLAGS="-O0 -g3 -W -Wall -DBUILD_WITH_CFUNC \
+#         -DBUILD_DEFAULT_CACHE -DBUILD_DEFAULT_TIMER"
+#     make
+#     sudo make install
+#
+
 MOD		:= zrtp
 $(MOD)_SRCS	+= zrtp.c
 $(MOD)_LFLAGS	+= -lzrtp -lbn
