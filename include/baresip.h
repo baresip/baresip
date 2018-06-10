bfe0eff61573f943413b9d9810e172f6431635aa
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2018-06-10 12:42:48 +0200

    aufilt: add pointer to "struct audio"
    This is needed so that the audio-filter (aufilt) modules can
    get access to the audio session state, and also the call object.
    
    This commit breaks the AUFILT api.

--- include/baresip.h
+++ include/baresip.h
@@ -468,12 +468,14 @@ struct aufilt_prm {
 };
 
 typedef int (aufilt_encupd_h)(struct aufilt_enc_st **stp, void **ctx,
-			      const struct aufilt *af, struct aufilt_prm *prm);
+			      const struct aufilt *af, struct aufilt_prm *prm,
+			      const struct audio *au);
 typedef int (aufilt_encode_h)(struct aufilt_enc_st *st,
 			      int16_t *sampv, size_t *sampc);
 
 typedef int (aufilt_decupd_h)(struct aufilt_dec_st **stp, void **ctx,
-			      const struct aufilt *af, struct aufilt_prm *prm);
+			      const struct aufilt *af, struct aufilt_prm *prm,
+			      const struct audio *au);
 typedef int (aufilt_decode_h)(struct aufilt_dec_st *st,
 			      int16_t *sampv, size_t *sampc);
 
