7642c8b2ae461fbb2a74e43556d2fa974c8bf1ae
Geoff Stewart <gstewart@martellotech.com>
2018-08-08 12:54:46 -0400

    Add the ability to specify the network interface on the command line

--- src/main.c
+++ src/main.c
@@ -59,6 +59,7 @@ static void usage(void)
 			 "\t-p <path>        Audio files\n"
 			 "\t-h -?            Help\n"
 			 "\t-t               Test and exit\n"
+			 "\t-n <net_if>      Specify network interface\n"
 			 "\t-u <parameters>  Extra UA parameters\n"
 			 "\t-v               Verbose debug\n"
 			 );
@@ -70,6 +71,7 @@ int main(int argc, char *argv[])
 	bool prefer_ipv6 = false, run_daemon = false, test = false;
 	const char *ua_eprm = NULL;
 	const char *execmdv[16];
+	const char *net_interface = NULL;
 	const char *audio_path = NULL;
 	const char *modv[16];
 	size_t execmdc = 0;
@@ -95,7 +97,7 @@ int main(int argc, char *argv[])
 
 #ifdef HAVE_GETOPT
 	for (;;) {
-		const int c = getopt(argc, argv, "6de:f:p:hu:vtm:");
+		const int c = getopt(argc, argv, "6de:f:p:hun:vtm:");
 		if (0 > c)
 			break;
 
@@ -148,6 +150,10 @@ int main(int argc, char *argv[])
 			test = true;
 			break;
 
+		case 'n':
+			net_interface = optarg;
+			break;
+
 		case 'u':
 			ua_eprm = optarg;
 			break;
@@ -172,9 +178,17 @@ int main(int argc, char *argv[])
 	}
 
 	/*
-	 * Initialise the top-level baresip struct, must be
-	 * done AFTER configuration is complete.
+	 * Set the network interface before initializing the config
 	 */
+	struct config *theconf = conf_config();
+	if (net_interface) {
+		str_ncpy(theconf->net.ifname, net_interface, sizeof(theconf->net.ifname));
+	}
+
+	/*
+	* Initialise the top-level baresip struct, must be
+	* done AFTER configuration is complete.
+	*/
 	err = baresip_init(conf_config(), prefer_ipv6);
 	if (err) {
 		warning("main: baresip init failed (%m)\n", err);
