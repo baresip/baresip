20ec6f10c37d8093adf15cfff1768a914c2c1976
Christian Spielberger <christian.spielberger@gmail.com>
2018-08-09 17:03:45 +0200

    audio: fix for ausrc_fb does not work anymore
    Signed-off-by: Wolfgang Netbal <w.netbal@commend.com>
    Signed-off-by: Christian Spielberger <c.spielberger@commend.com>
    Signed-off-by: Johannes Natter <office@dsp-crowd.com>
    Signed-off-by: Christian Spielberger <c.spielberger@commend.com>

--- src/audio.c
+++ src/audio.c
@@ -191,6 +191,7 @@ static void stop_tx(struct autx *tx, struct audio *a)
 	tx->ausrc_fb = true;
 	tx->ausrc = mem_deref(tx->ausrc);
 	tx->aubuf = mem_deref(tx->aubuf);
+	tx->ausrc_fb = false;
 
 	list_flush(&tx->filtl);
 }
