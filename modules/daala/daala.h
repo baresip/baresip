9c5d7173daaec3426d1c8160b3647f65d8727614
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2018-09-02 21:40:58 +0200

    daala: update vidcodec API

--- modules/daala/daala.h
+++ modules/daala/daala.h
@@ -10,7 +10,7 @@ int daala_encode_update(struct videnc_state **vesp, const struct vidcodec *vc,
 			struct videnc_param *prm, const char *fmtp,
 			videnc_packet_h *pkth, void *arg);
 int daala_encode(struct videnc_state *ves, bool update,
-		 const struct vidframe *frame);
+		 const struct vidframe *frame, uint64_t timestamp);
 
 
 /* Decode */
