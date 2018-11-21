f68de5fbd278c678755d7637c95abccc879ced5c
Ola Palm <ola.palm@me.com>
2018-11-21 10:14:15 +0100

    current file is always stopped when playfile is called. audio.c checked out from Alfred due to some merge conflict.

--- modules/debug_cmd/debug_cmd.c
+++ modules/debug_cmd/debug_cmd.c
@@ -81,27 +81,31 @@ static int cmd_ua_debug(struct re_printf *pf, void *unused)
 		return re_hprintf(pf, "(no user-agent)\n");
 }
 
-
 static int cmd_play_file(struct re_printf *pf, void *arg)
 {
+	static struct play *g_play;
 	struct cmd_arg *carg = arg;
 	const char *filename = carg->prm;
 	int err;
+	/* Stop the current tone, if any */
+	g_play = mem_deref(g_play);
 
-	err = re_hprintf(pf, "playing audio file \"%s\" ..\n", filename);
-	if (err)
-		return err;
+	if (str_isset(filename))
+	{
+		err = re_hprintf(pf, "playing audio file \"%s\" ..\n", filename);
+		if (err)
+			return err;
 
-	err = play_file(NULL, baresip_player(), filename, 0);
-	if (err) {
-		warning("debug_cmd: play_file(%s) failed (%m)\n",
-			filename, err);
-		return err;
+		err = play_file(&g_play, baresip_player(), filename, 0);
+		if (err)
+		{
+			warning("debug_cmd: play_file(%s) failed        (%m)\n",
+					filename, err);
+			return err;
+		}
 	}
-
 	return err;
 }
-
 
 static int reload_config(struct re_printf *pf, void *arg)
 {
