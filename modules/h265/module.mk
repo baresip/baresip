415d7d84e4de7e676bb024a2cde0348a83dd09c2
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2019-03-20 13:22:08 +0100

    h265: one file per line

--- modules/h265/module.mk
+++ modules/h265/module.mk
@@ -5,7 +5,10 @@
 #
 
 MOD		:= h265
-$(MOD)_SRCS	+= h265.c encode.c decode.c fmt.c
+$(MOD)_SRCS	+= decode.c
+$(MOD)_SRCS	+= encode.c
+$(MOD)_SRCS	+= fmt.c
+$(MOD)_SRCS	+= h265.c
 $(MOD)_LFLAGS	+= `pkg-config --libs libavcodec libavutil`
 
 include mk/mod.mk
