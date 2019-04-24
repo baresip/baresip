2d2d2a6f7d4fc9ca31f68cbc2f929eb262c0c895
premultiply <ulrich.peters@posteo.de>
2019-04-24 08:52:57 +0200

    Interop: Parameters reordering and whitespace removal

--- modules/mpa/mpa.c
+++ modules/mpa/mpa.c
@@ -106,6 +106,34 @@ static int module_init(void)
 
 	strcpy(mode,mpa.fmtp);
 
+        if (0 == conf_get_u32(conf, "mpa_layer", &value)) {
+                if (value<1 || value>4) {
+                        warning("MPA layer 1, 2 or 3 are allowed.");
+                        return -1;
+                }
+                (void)re_snprintf(fmtp+strlen(fmtp),
+                        sizeof(fmtp)-strlen(fmtp),
+                        ";layer=%d", value);
+        }
+        if (0 == conf_get_u32(conf, "mpa_samplerate", &value)) {
+                switch (value) {
+                case 32000:
+                case 44100:
+                case 48000:
+                case 16000:
+                case 22050:
+                case 24000:
+                        break;
+                default:
+                        warning("MPA samplerates of 16, 22.05, 24, 32, "
+                                "44.1, and 48 kHz are allowed.\n");
+                        return -1;
+                }
+                (void)re_snprintf(fmtp+strlen(fmtp),
+                        sizeof(fmtp)-strlen(fmtp),
+                        ";samplerate=%d", value);
+        }
+
 	if (0 == conf_get_u32(conf, "mpa_bitrate", &value)) {
 		if (value<8000 || value>384000) {
 			warning("MPA bitrate between 8000 and "
@@ -115,34 +143,7 @@ static int module_init(void)
 
 		(void)re_snprintf(fmtp+strlen(fmtp),
 			sizeof(fmtp)-strlen(fmtp),
-			"; bitrate=%d", value);
-	}
-	if (0 == conf_get_u32(conf, "mpa_layer", &value)) {
-		if (value<1 || value>4) {
-			warning("MPA layer 1, 2 or 3 are allowed.");
-			return -1;
-		}
-		(void)re_snprintf(fmtp+strlen(fmtp),
-			sizeof(fmtp)-strlen(fmtp),
-			"; layer=%d", value);
-	}
-	if (0 == conf_get_u32(conf, "mpa_samplerate", &value)) {
-		switch (value) {
-		case 32000:
-		case 44100:
-		case 48000:
-		case 16000:
-		case 22050:
-		case 24000:
-			break;
-		default:
-			warning("MPA samplerates of 16, 22.05, 24, 32, "
-				"44.1, and 48 kHz are allowed.\n");
-			return -1;
-		}
-		(void)re_snprintf(fmtp+strlen(fmtp),
-			sizeof(fmtp)-strlen(fmtp),
-			"; samplerate=%d", value);
+			";bitrate=%d", value);
 	}
 	if (0 == conf_get_str(conf, "mpa_mode", mode, sizeof(mode))) {
 		char *p = mode;
@@ -162,11 +163,11 @@ static int module_init(void)
 
 		(void)re_snprintf(fmtp+strlen(fmtp),
 			sizeof(fmtp)-strlen(fmtp),
-			"; mode=%s", mode);
+			";mode=%s", mode);
 	}
 
-	if (fmtp[0]==';' && fmtp[1]==' ')
-		mpa.fmtp = fmtp+2;
+	if (fmtp[0]==';')
+		mpa.fmtp = fmtp+1;
 	else
 		mpa.fmtp = fmtp;
 
