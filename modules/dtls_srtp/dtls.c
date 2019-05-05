213d2cb0eaa0660c4b7018da9ce619def7093263
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2019-05-05 16:14:29 +0200

    dtls_srtp: remove dtls_print_sha1_fingerprint

--- modules/dtls_srtp/dtls.c
+++ modules/dtls_srtp/dtls.c
@@ -9,27 +9,6 @@
 #include "dtls_srtp.h"
 
 
-int dtls_print_sha1_fingerprint(struct re_printf *pf, const struct tls *tls)
-{
-	uint8_t md[20];
-	unsigned int i;
-	int err = 0;
-
-	if (!tls)
-		return EINVAL;
-
-	err = tls_fingerprint(tls, TLS_FINGERPRINT_SHA1, md, sizeof(md));
-	if (err)
-		return err;
-
-	for (i=0; i<sizeof(md); i++) {
-		err |= re_hprintf(pf, "%s%02X", i==0 ? "" : ":", md[i]);
-	}
-
-	return err;
-}
-
-
 int dtls_print_sha256_fingerprint(struct re_printf *pf, const struct tls *tls)
 {
 	uint8_t md[32];
