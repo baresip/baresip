878529443f0232c2f9c26ceb5e86dfb37d86f646
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2019-08-03 12:59:28 +0200

    fix travis

--- modules/avcodec/decode.c
+++ modules/avcodec/decode.c
@@ -86,6 +86,7 @@ static inline void fragment_rewind(struct viddec_state *vds)
 }
 
 
+#if LIBAVUTIL_VERSION_MAJOR >= 56
 static enum AVPixelFormat get_hw_format(AVCodecContext *ctx,
                                         const enum AVPixelFormat *pix_fmts)
 {
@@ -99,6 +100,7 @@ static enum AVPixelFormat get_hw_format(AVCodecContext *ctx,
        fprintf(stderr, "Failed to get HW surface format.\n");
        return AV_PIX_FMT_NONE;
 }
+#endif
 
 
 static int init_decoder(struct viddec_state *st, const char *name)
@@ -129,17 +131,18 @@ static int init_decoder(struct viddec_state *st, const char *name)
 	if (!st->ctx || !st->pict)
 		return ENOMEM;
 
+#if LIBAVUTIL_VERSION_MAJOR >= 56
 	/* Hardware accelleration */
-       if (hw_device_ctx) {
-               st->ctx->hw_device_ctx = av_buffer_ref(hw_device_ctx);
-               st->ctx->get_format = get_hw_format;
+	if (hw_device_ctx) {
+		st->ctx->hw_device_ctx = av_buffer_ref(hw_device_ctx);
+		st->ctx->get_format = get_hw_format;
 
-               info("avcodec: decode: hardware accel enabled\n");
-       }
-       else {
-               info("avcodec: decode: hardware accel disabled\n");
-       }
-
+		info("avcodec: decode: hardware accel enabled\n");
+	}
+	else {
+		info("avcodec: decode: hardware accel disabled\n");
+	}
+#endif
 
 	if (avcodec_open2(st->ctx, st->codec, NULL) < 0)
 		return ENOENT;
@@ -247,6 +250,7 @@ static int ffdecode(struct viddec_state *st, struct vidframe *frame)
 
 	if (got_picture) {
 
+#if LIBAVUTIL_VERSION_MAJOR >= 56
 		if (hw_frame) {
 			/* retrieve data from GPU to CPU */
 			ret = av_hwframe_transfer_data(st->pict, hw_frame, 0);
@@ -256,6 +260,7 @@ static int ffdecode(struct viddec_state *st, struct vidframe *frame)
 				goto out;
 			}
 		}
+#endif
 
 		frame->fmt = avpixfmt_to_vidfmt(st->pict->format);
 		if (frame->fmt == (enum vidfmt)-1) {
