81396d01878e03f96bc898498c7d294436d239b7
premultiply <ulrich.peters@posteo.de>
2019-11-25 14:04:59 +0100

    manual custom baresip style formating

--- modules/aptx/encode.c
+++ modules/aptx/encode.c
@@ -26,7 +26,7 @@ static void destructor(void *arg)
 
 
 int aptx_encode_update(struct auenc_state **aesp, const struct aucodec *ac,
-					   struct auenc_param *param, const char *fmtp)
+                       struct auenc_param *param, const char *fmtp)
 {
 	struct auenc_state *aes;
 	int err = 0;
@@ -63,7 +63,7 @@ out:
 
 
 int aptx_encode_frm(struct auenc_state *aes, uint8_t *buf, size_t *len,
-					int fmt, const void *sampv, size_t sampc)
+                    int fmt, const void *sampv, size_t sampc)
 {
 	size_t processed = 0;
 	size_t written = 0;
@@ -92,15 +92,15 @@ int aptx_encode_frm(struct auenc_state *aes, uint8_t *buf, size_t *len,
 		intermediate_buf[s * APTX_WORDSIZE + 2] = s16[s * 2 + 1];
 	}
 
-	processed = aptx_encode(aes->enc, intermediate_buf, intermediate_len, buf,
-							*len, &written);
+	processed = aptx_encode(aes->enc, intermediate_buf, intermediate_len,
+	                        buf, *len, &written);
 
 	free(intermediate_buf);
 
 	if (processed != intermediate_len)
 		warning("aptx: Encoding stopped in the middle of the sample, "
-				"dropped %u bytes\n",
-				(unsigned int)(intermediate_len - processed));
+		        "dropped %u bytes\n",
+		        (unsigned int)(intermediate_len - processed));
 
 	*len = written;
 
