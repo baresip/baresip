be32c1638a6c3b3780224243d1aad987567267b3
premultiply <ulrich.peters@posteo.de>
2019-11-25 10:09:16 +0100

    Reformat

--- modules/aptx/decode.c
+++ modules/aptx/decode.c
@@ -4,90 +4,91 @@
  * Copyright (C) 2019 Hessischer Rundfunk
  */
 
-#include <re.h>
-#include <rem.h>
+#include "aptx.h"
 #include <baresip.h>
 #include <openaptx.h>
+#include <re.h>
+#include <rem.h>
 #include <stdlib.h>
-#include "aptx.h"
 
-struct audec_state
-{
-	struct aptx_context *dec;
+struct audec_state {
+  struct aptx_context *dec;
 };
 
-static void destructor(void *arg)
-{
-	struct audec_state *ads = arg;
 
-	if (ads->dec)
-		aptx_finish(ads->dec);
+static void destructor(void *arg) {
+  struct audec_state *ads = arg;
+
+  if (ads->dec)
+    aptx_finish(ads->dec);
 }
 
-int aptx_decode_update(struct audec_state **adsp, const struct aucodec *ac, const char *fmtp)
-{
-	struct audec_state *ads;
-	int err = 0;
-	(void)fmtp;
 
-	if (!adsp || !ac || !ac->ch)
-		return EINVAL;
+int aptx_decode_update(struct audec_state **adsp, const struct aucodec *ac,
+                       const char *fmtp) {
+  struct audec_state *ads;
+  int err = 0;
 
-	ads = *adsp;
+  (void)fmtp;
 
-	if (ads)
-		return 0;
+  if (!adsp || !ac || !ac->ch)
+    return EINVAL;
 
-	ads = mem_zalloc(sizeof(*ads), destructor);
-	if (!ads)
-		return ENOMEM;
+  ads = *adsp;
 
-	ads->dec = aptx_init(APTX_VARIANT);
-	if (!ads->dec)
-	{
-		warning("Cannot initialize aptX decoder.\n");
-		err = ENOMEM;
-		goto out;
-	}
+  if (ads)
+    return 0;
+
+  ads = mem_zalloc(sizeof(*ads), destructor);
+  if (!ads)
+    return ENOMEM;
+
+  ads->dec = aptx_init(APTX_VARIANT);
+  if (!ads->dec) {
+    warning("aptx: Cannot initialize decoder.\n");
+    err = ENOMEM;
+    goto out;
+  }
 
 out:
-	if (err)
-		mem_deref(ads);
-	else
-		*adsp = ads;
+  if (err)
+    mem_deref(ads);
+  else
+    *adsp = ads;
 
-	return err;
+  return err;
 }
 
-int aptx_decode_frm(struct audec_state *ads, int fmt, void *sampv, size_t *sampc, const uint8_t *buf, size_t len)
-{
-	size_t processed = 0;
-	size_t written = 0;
 
-	uint8_t *s16 = sampv;
+int aptx_decode_frm(struct audec_state *ads, int fmt, void *sampv,
+                    size_t *sampc, const uint8_t *buf, size_t len) {
+  size_t processed = 0;
+  size_t written = 0;
 
-	if (!ads || !sampv || !sampc || !buf)
-		return EINVAL;
+  uint8_t *s16 = sampv;
 
-	if (fmt != AUFMT_S16LE)
-		return ENOTSUP;
+  if (!ads || !sampv || !sampc || !buf)
+    return EINVAL;
 
-	processed = aptx_decode(ads->dec, buf, len, s16, *sampc, &written);
+  if (fmt != AUFMT_S16LE)
+    return ENOTSUP;
 
-	*sampc = written / APTX_WORDSIZE;
+  processed = aptx_decode(ads->dec, buf, len, s16, *sampc, &written);
 
-	if (processed != len)
-		warning("aptX decoding stopped in the middle of the sample, dropped %u bytes\n", (unsigned int)(len - processed));
+  *sampc = written / APTX_WORDSIZE;
 
-	if (*sampc > 0)
-	{
-		/* remap S24 to S16 in same buffer */
-		for (size_t s = 0; s < *sampc; s++)
-		{
-			s16[s * 2]     = s16[(s * APTX_WORDSIZE) + 1];
-			s16[s * 2 + 1] = s16[(s * APTX_WORDSIZE) + 2];
-		}
-	}
+  if (processed != len)
+    warning("aptx: Decoding stopped in the middle of the sample, "
+            "dropped %u bytes\n",
+            (unsigned int)(len - processed));
 
-	return 0;
+  if (*sampc > 0) {
+    /* remap S24 to S16 in same buffer */
+    for (size_t s = 0; s < *sampc; s++) {
+      s16[s * 2] = s16[(s * APTX_WORDSIZE) + 1];
+      s16[s * 2 + 1] = s16[(s * APTX_WORDSIZE) + 2];
+    }
+  }
+
+  return 0;
 }
