e90ab3f7e0b22f5bab7087d7126bb64257bdc5d2
premultiply <ulrich.peters@posteo.de>
2019-11-25 10:10:26 +0100

    Reformat

--- modules/aptx/sdp.c
+++ modules/aptx/sdp.c
@@ -4,53 +4,56 @@
  * Copyright (C) 2019 Hessischer Rundfunk
  */
 
-#include <re.h>
+#include "aptx.h"
 #include <baresip.h>
 #include <openaptx.h>
-#include "aptx.h"
+#include <re.h>
 
-static unsigned param_value(const char *fmtp, const char *name)
-{
-	struct pl pl, val;
 
-	if (!fmtp || !name)
-		return 0;
+static unsigned param_value(const char *fmtp, const char *name) {
+  struct pl pl, val;
 
-	pl_set_str(&pl, fmtp);
+  if (!fmtp || !name)
+    return 0;
 
-	if (fmt_param_get(&pl, name, &val))
-		return pl_u32(&val);
+  pl_set_str(&pl, fmtp);
 
-	return 0;
+  if (fmt_param_get(&pl, name, &val))
+    return pl_u32(&val);
+
+  return 0;
 }
 
-int aptx_fmtp_enc(struct mbuf *mb, const struct sdp_format *fmt, bool offer, void *arg)
-{
-	(void)offer;
-	(void)arg;
 
-	if (!mb || !fmt)
-		return 0;
+int aptx_fmtp_enc(struct mbuf *mb, const struct sdp_format *fmt, bool offer,
+                  void *arg) {
+  (void)offer;
+  (void)arg;
 
-	return mbuf_printf(mb, "a=fmtp:%s "
-						   "variant=%s; bitresolution=%u;\r\n",
-					   fmt->id, APTX_VARIANT == APTX_VARIANT_HD ? "hd" : "standard", APTX_VARIANT == APTX_VARIANT_HD ? 24 : 16);
+  if (!mb || !fmt)
+    return 0;
+
+  return mbuf_printf(mb,
+                     "a=fmtp:%s "
+                     "variant=%s; bitresolution=%u;\r\n",
+                     fmt->id,
+                     APTX_VARIANT == APTX_VARIANT_HD ? "hd" : "standard",
+                     APTX_VARIANT == APTX_VARIANT_HD ? 24 : 16);
 }
 
-bool aptx_fmtp_cmp(const char *lfmtp, const char *rfmtp, void *arg)
-{
-	warning("sdp: rfmtp: %s\n", rfmtp);
 
-	(void)lfmtp;
-	(void)arg;
+bool aptx_fmtp_cmp(const char *lfmtp, const char *rfmtp, void *arg) {
+  (void)lfmtp;
+  (void)arg;
 
-	/*
-	if (param_value(rfmtp, "variant") != APTX_VARIANT)
-		return false;
-	*/
+  /*
+  if (param_value(rfmtp, "variant") != APTX_VARIANT)
+          return false;
+  */
 
-	if (param_value(rfmtp, "bitresolution") != (APTX_VARIANT == APTX_VARIANT_HD ? 24 : 16))
-		return false;
+  if (param_value(rfmtp, "bitresolution") !=
+      (APTX_VARIANT == APTX_VARIANT_HD ? 24 : 16))
+    return false;
 
-	return true;
+  return true;
 }
