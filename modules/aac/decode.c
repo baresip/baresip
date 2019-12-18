9ea74002bbad3c15ad380fa399e8281bd23371cc
premultiply <ulrich.peters@posteo.de>
2019-12-18 16:03:18 +0100

    AAC mpeg4-generic audio
    Supporting AAC-LC, AAC-LD, AAC-ELD HE-AAC and HE-AACv2

--- modules/aac/decode.c
+++ modules/aac/decode.c
@@ -1,7 +1,8 @@
 /**
- * @file aac/decode.c AAC Decoder
+ * @file aac/decode.c MPEG-4 AAC Decoder
  *
  * Copyright (C) 2010 Creytiv.com
+ * Copyright (C) 2019 Hessischer Rundfunk
  */
 
 #include <string.h>
@@ -10,6 +11,13 @@
 #include <baresip.h>
 #include <fdk-aac/aacdecoder_lib.h>
 #include "aac.h"
+
+
+struct au_hdr {
+	uint16_t offset;
+	uint16_t size;
+	uint16_t count;
+};
 
 
 struct audec_state {
@@ -26,12 +34,60 @@ static void destructor(void *arg)
 }
 
 
+static int hdr_decode(struct au_hdr *au_data, const uint8_t *p,
+                      const size_t plen)
+{
+	uint16_t au_headers_length;
+	uint16_t au_data_offset;
+	uint16_t au_data_length;
+	uint16_t bits;
+
+	if (plen < sizeof(uint16_t) * 2)
+		return EPROTO;
+
+	au_headers_length = ntohs(*(uint16_t *)(void *)&p[0]);
+	au_data_offset = sizeof(uint16_t) + (au_headers_length / 8);
+	au_data_length = plen - au_data_offset;
+	au_data->count = (au_headers_length / (sizeof(uint16_t) * 8));
+
+	au_data->offset = au_data_offset;
+
+	bits = ntohs(*(uint16_t *)(void *)&p[2]);
+
+	au_data->size = bits >> ((sizeof(uint16_t) * 8) - SIZELENGTH);
+
+	if (au_data->size == 0) {
+		warning("aac: decode: invalid access unit size (zero)\n",
+		        au_data->size);
+		return EBADMSG;
+	}
+
+	if (au_data->size > au_data_length) {
+		debug("aac: decode: fragmented access unit "
+		      "(au-data-size: %zu > packet-data-size: %zu)\n",
+		      au_data->size, au_data_length);
+	}
+
+	if (au_data->size != au_data_length) {
+		debug("aac: decode: multiple access units per packet (%zu)\n",
+		      au_data->count);
+	}
+
+	return 0;
+}
+
+
 int aac_decode_update(struct audec_state **adsp, const struct aucodec *ac,
-		       const char *fmtp)
+                      const char *fmtp)
 {
 	struct audec_state *ads;
+	AAC_DECODER_ERROR error;
 	int err = 0;
 	(void)fmtp;
+
+	struct pl config;
+	char config_str[64];
+	uint8_t config_bin[32];
 
 	if (!adsp || !ac || !ac->ch)
 		return EINVAL;
@@ -45,14 +101,48 @@ int aac_decode_update(struct audec_state **adsp, const struct aucodec *ac,
 	if (!ads)
 		return ENOMEM;
 
-	ads->dec = aacDecoder_Open(TT_MP4_LATM_MCP1, 1);
+	ads->dec = aacDecoder_Open(TT_MP4_RAW, 1);
 	if (!ads->dec) {
 		warning("aac: error opening decoder\n");
 		err = ENOMEM;
 		goto out;
 	}
 
- out:
+	info("aac: decode update: fmtp='%s'\n", fmtp);
+
+	err = re_regex(fmtp, str_len(fmtp), "config=[0-9a-f]+", &config);
+	if (err)
+		goto out;
+
+	err = pl_strcpy(&config, config_str, sizeof(config_str));
+	if (err)
+		goto out;
+
+	err = str_hex(config_bin, strlen(config_str)/2, config_str);
+	if (err)
+		goto out;
+
+	UCHAR *conf = config_bin;
+	const UINT length = (UINT)strlen(config_str)/2;
+
+	error = aacDecoder_ConfigRaw(ads->dec, &conf, &length);
+	if (error != AAC_DEC_OK) {
+		warning("aac: decode: set config error (0x%x)\n", error);
+		err = EPROTO;
+		goto out;
+	}
+
+	error  = aacDecoder_SetParam(ads->dec, AAC_PCM_MIN_OUTPUT_CHANNELS,
+	                             aac_channels);
+	error |= aacDecoder_SetParam(ads->dec, AAC_PCM_MAX_OUTPUT_CHANNELS,
+	                             aac_channels);
+	if (error != AAC_DEC_OK) {
+		warning("aac: decode: set param error (0x%x)\n", error);
+		err = EINVAL;
+		goto out;
+	}
+
+out:
 	if (err)
 		mem_deref(ads);
 	else
@@ -62,18 +152,19 @@ int aac_decode_update(struct audec_state **adsp, const struct aucodec *ac,
 }
 
 
-int aac_decode_frm(struct audec_state *ads,
-		   int fmt, void *sampv, size_t *sampc,
-		   bool marker, const uint8_t *buf, size_t len)
+int aac_decode_frm(struct audec_state *ads, int fmt, void *sampv,
+                   size_t *sampc, bool marker, const uint8_t *buf, size_t len)
 {
 	UCHAR *pBuffer = (UCHAR *)buf;
-	UINT bufferSize = (UINT)len;
-	UINT valid = (UINT)len;
+	UINT bufferSize = 0;
+	UINT valid = 0;
 	AAC_DECODER_ERROR error;
+	INT size;
+	int err = 0;
 	size_t nsamp = 0;
-	unsigned i;
 	int16_t *s16 = sampv;
-	int size;
+	size_t pos = 0;
+	struct au_hdr au_data;
 	(void)marker;
 
 	if (!ads || !sampv || !sampc || !buf)
@@ -82,47 +173,68 @@ int aac_decode_frm(struct audec_state *ads,
 	if (fmt != AUFMT_S16LE)
 		return ENOTSUP;
 
-	error = aacDecoder_Fill(ads->dec, &pBuffer, &bufferSize, &valid);
-	if (error != AAC_DEC_OK) {
-		warning("aac: aacDecoder_Fill() failed: 0x%x\n", error);
-		return EPROTO;
-	}
+	err = hdr_decode(&au_data, buf, len);
+	if (err)
+		return err;
 
-	size = (int)*sampc;
+	pos = au_data.offset;
+	pBuffer += pos;
 
-	for (i=0; i<8; i++) {
-
+	while (len > pos) {
 		CStreamInfo *info;
 
-		error = aacDecoder_DecodeFrame(ads->dec, &s16[nsamp], size, 0);
-		if (error == AAC_DEC_NOT_ENOUGH_BITS)
-			break;
+		bufferSize = len - pos;
+		valid = bufferSize;
 
+		error =
+		    aacDecoder_Fill(ads->dec, &pBuffer, &bufferSize, &valid);
 		if (error != AAC_DEC_OK) {
-			warning("aac: aacDecoder_DecodeFrame() failed: 0x%x\n",
-				error);
+			warning("aac: aacDecoder_Fill() failed (0x%x)\n",
+			        error);
+			return EPROTO;
+		}
+
+		size = (INT)*sampc;
+
+		error =
+		    aacDecoder_DecodeFrame(ads->dec, &s16[nsamp], size, 0);
+		if (error == AAC_DEC_NOT_ENOUGH_BITS) {
+			warning("aac: aacDecoder_DecodeFrame() failed: "
+			        "NOT ENOUGH BITS %u / %u\n",
+			        bufferSize, valid);
+			break;
+		}
+		if (error != AAC_DEC_OK) {
+			warning(
+			    "aac: aacDecoder_DecodeFrame() failed (0x%x)\n",
+			    error);
 			return EPROTO;
 		}
 
 		info = aacDecoder_GetStreamInfo(ads->dec);
 		if (!info) {
-			warning("aac: Unable to get stream info\n");
+			warning("aac: decode: unable to get stream info\n");
 			return EBADMSG;
 		}
 
-		if (info->sampleRate != AAC_SRATE) {
-			warning("aac: decode samplerate mismatch (%d != %d)\n",
-				info->sampleRate, AAC_SRATE);
+		if (info->sampleRate != (INT)aac_samplerate) {
+			warning(
+			    "aac: decode: samplerate mismatch (%d != %d)\n",
+			    info->sampleRate, aac_samplerate);
 			return EPROTO;
 		}
-		if (info->numChannels != AAC_CHANNELS) {
-			warning("aac: decode channels mismatch (%d != %d)\n",
-				info->numChannels, AAC_CHANNELS);
+		if (info->numChannels != (INT)aac_channels) {
+			warning(
+			    "aac: decode: channels mismatch (%d != %d)\n",
+			    info->numChannels, aac_channels);
 			return EPROTO;
 		}
 
 		nsamp += (info->frameSize * info->numChannels);
-		size  -= (info->frameSize * info->numChannels);
+		size -= (info->frameSize * info->numChannels);
+
+		pos += bufferSize - valid;
+		pBuffer += bufferSize - valid;
 	}
 
 	if (nsamp > *sampc)
--- modules/aac/encode.c
+++ modules/aac/encode.c
@@ -1,7 +1,8 @@
 /**
- * @file aac/encode.c AAC Encoder
+ * @file aac/encode.c MPEG-4 AAC Encoder
  *
  * Copyright (C) 2010 Creytiv.com
+ * Copyright (C) 2019 Hessischer Rundfunk
  */
 
 #include <re.h>
@@ -9,11 +10,6 @@
 #include <baresip.h>
 #include <fdk-aac/aacenc_lib.h>
 #include "aac.h"
-
-
-enum {
-	FRAME_SIZE = 480
-};
 
 
 struct auenc_state {
@@ -30,54 +26,240 @@ static void destructor(void *arg)
 }
 
 
+static void hdr_encode(uint8_t *p, uint16_t size)
+{
+	uint8_t n = 1; /* we only have a single AU-header!!! */
+	/* +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+- .. -+-+-+-+-+-+-+-+-+-+
+	 * |AU-headers-length|AU-header|AU-header|      |AU-header|padding|
+	 * |                 |   (1)   |   (2)   |      |   (n)   | bits  |
+	 * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+- .. -+-+-+-+-+-+-+-+-+-+
+	 */
+
+	/* AU-headers-length */
+	*(uint16_t *)(void *)&p[0] = htons(sizeof(uint16_t) * 8 * n);
+
+	/* +---------------------------------------+
+	 * |     AU-size                           |
+	 * +---------------------------------------+
+	 * |     AU-Index / AU-Index-delta         |
+	 * +---------------------------------------+
+	 * |     CTS-flag                          |
+	 * +---------------------------------------+
+	 * |     CTS-delta                         |
+	 * +---------------------------------------+
+	 * |     DTS-flag                          |
+	 * +---------------------------------------+
+	 * |     DTS-delta                         |
+	 * +---------------------------------------+
+	 * |     RAP-flag                          |
+	 * +---------------------------------------+
+	 * |     Stream-state                      |
+	 * +---------------------------------------+
+	 */
+
+	/* The AU-header, no CTS, DTS, RAP, Stream-state
+	 *
+	 * AU-size is always the total size of the AU, not the fragmented
+	 * size
+	 */
+	*(uint16_t *)(void *)&p[2] =
+	    htons(size << ((sizeof(uint16_t) * 8) - SIZELENGTH));
+}
+
+
 int aac_encode_update(struct auenc_state **aesp, const struct aucodec *ac,
-		      struct auenc_param *param, const char *fmtp)
+                      struct auenc_param *param, const char *fmtp)
 {
 	struct auenc_state *aes;
+	struct aac_param prm;
+	uint32_t enc_aot, enc_ratio;
+	AACENC_InfoStruct enc_info;
 	AACENC_ERROR error;
 	int err = 0;
-
 	(void)param;
 	(void)fmtp;
 
 	if (!aesp || !ac || !ac->ch)
 		return EINVAL;
 
+	debug("aac: encoder fmtp (%s)\n", fmtp);
+
+	if (str_isset(fmtp)) {
+		aac_mirror_params(fmtp);
+		aac_decode_fmtp(&prm, fmtp);
+	}
+	else {
+		prm.profile_level_id = aac_profile;
+		prm.bitrate = aac_bitrate;
+		prm.constantduration = aac_constantduration;
+	}
+
 	aes = *aesp;
 
-	if (aes)
-		goto out;
+	if (!aes) {
+		aes = mem_zalloc(sizeof(*aes), destructor);
+		if (!aes)
+			return ENOMEM;
 
-	aes = mem_zalloc(sizeof(*aes), destructor);
-	if (!aes)
-		return ENOMEM;
+		error = aacEncOpen(&aes->enc, 0, 0);
+		if (error != AACENC_OK) {
+			warning("aac: Unable to open the encoder (0x%x)\n",
+			        error);
+			err = ENOMEM;
+			goto out;
+		}
 
-	error = aacEncOpen(&aes->enc, 0, AAC_CHANNELS);
-	if (error != AACENC_OK) {
-		warning("aac: Unable to open the encoder: 0x%x\n",
-			error);
-		err = ENOMEM;
-		goto out;
+		if ((prm.profile_level_id >= 14 &&
+		    prm.profile_level_id <= 21) ||
+		    (prm.profile_level_id >= 40 &&
+		    prm.profile_level_id <= 43)) {
+			info("aac: Encoder Profile AAC-LC\n");
+			enc_aot = AOT_AAC_LC; /* AAC-LC */
+			enc_ratio = 1;
+		}
+		else if (prm.profile_level_id == 52 ||
+		         (prm.profile_level_id >= 22 &&
+		          prm.profile_level_id <= 29)) {
+			info("aac: Encoder Profile AAC-LD\n");
+			enc_aot = AOT_ER_AAC_LD; /* AAC-LD */
+			enc_ratio = 1;
+		}
+		else if (prm.profile_level_id >= 76 &&
+		         prm.profile_level_id <= 77) {
+			info("aac: Encoder Profile AAC-ELD\n");
+			enc_aot = AOT_ER_AAC_ELD; /* AAC-ELD */
+			enc_ratio = 1;
+			switch (ac->ch) {
+			case MODE_1: /* mono */
+				prm.profile_level_id = 76;
+				break;
+			case MODE_2: /* stereo */
+				prm.profile_level_id = 77;
+				break;
+			}
+		}
+		else if (prm.profile_level_id >= 44 &&
+		         prm.profile_level_id <= 47) {
+			info("aac: Encoder Profile HE-AAC\n");
+			enc_aot = AOT_SBR; /* HE-AAC */
+			enc_ratio = 2; /* SBR */
+		}
+		else if (prm.profile_level_id >= 48 &&
+		         prm.profile_level_id <= 51 &&
+			 ac->ch == MODE_2) {
+			info("aac: Encoder Profile HE-AAC v2\n");
+			enc_aot = AOT_PS; /* HE-AAC v2 */
+			enc_ratio = 2; /* SBR */
+		}
+		else {
+			err = EINVAL;
+			goto out;
+		}
+
+		debug("srate: %u, crate: %u, ch: %u, pch: %u, ptime: %u\n",
+		      ac->srate, ac->crate, ac->ch, ac->pch, ac->ptime);
+
+		/* set mandatory encoder params: */
+		error |= aacEncoder_SetParam(aes->enc, AACENC_AOT,
+		                             enc_aot);
+		error |= aacEncoder_SetParam(aes->enc, AACENC_SAMPLERATE,
+		                             ac->srate);
+		error |= aacEncoder_SetParam(aes->enc, AACENC_CHANNELMODE,
+		                             ac->ch);
+		error |= aacEncoder_SetParam(aes->enc, AACENC_BITRATE,
+		                             prm.bitrate);
+		error |= aacEncoder_SetParam(aes->enc, AACENC_TRANSMUX,
+		                             TT_MP4_RAW);
+		/* set object specific encoder params: */
+		error |= aacEncoder_SetParam(aes->enc, AACENC_GRANULE_LENGTH,
+		                             prm.constantduration/enc_ratio);
+		/* set optional encoder params: */
+		error |= aacEncoder_SetParam(aes->enc, AACENC_BITRATEMODE,
+		                             0); /* AACENC_BR_MODE_CBR */
+		error |= aacEncoder_SetParam(aes->enc, AACENC_AFTERBURNER,
+		                             1);
+		if (error != AACENC_OK) {
+			err = EINVAL;
+			goto out;
+		}
+
+		error = aacEncEncode(aes->enc, NULL, NULL, NULL, NULL);
+		if (error != AACENC_OK) {
+			warning(
+			    "aac: Unable to initialize the encoder (0x%x)\n",
+			    error);
+			err = EINVAL;
+			goto out;
+		}
+
+		error = aacEncInfo(aes->enc, &enc_info);
+		if (error != AACENC_OK) {
+			warning(
+			    "aac: Failed to get AAC encoder info (0x%x)\n",
+			    error);
+			err = EINVAL;
+			goto out;
+		}
+
+		re_snprintf(prm.config, sizeof(prm.config), "%w",
+		            enc_info.confBuf, enc_info.confSize);
+
+		prm.constantduration = enc_info.frameLength;
+		prm.bitrate = aacEncoder_GetParam(aes->enc, AACENC_BITRATE);
+
+		info("aac: Encoder configuration: conf=%w, "
+		     "frameLength=%u, inputChannels=%u, nDelay=%u\n",
+		     enc_info.confBuf, enc_info.confSize,
+		     enc_info.frameLength, enc_info.inputChannels,
+		     enc_info.nDelay);
+
+		debug("aac: encoder setup:\n"
+		      "\tAOT=%u\n"
+		      "\tBITRATE=%u\n"
+		      "\tBITRATEMODE=%u\n"
+		      "\tSAMPLERATE=%u\n"
+		      "\tSBR_MODE=%u\n"
+		      "\tGRANULE_LENGTH=%u\n"
+		      "\tCHANNELMODE=%u\n"
+		      "\tCHANNELORDER=%u\n"
+		      "\tSBR_RATIO=%u\n"
+		      "\tAFTERBURNER=%u\n"
+		      "\tBANDWIDTH=%u\n"
+		      "\tPEAK_BITRATE=%u\n"
+		      "\tTRANSMUX=%u\n"
+		      "\tHEADER PERIOD=%u\n"
+		      "\tSIGNALING_MODE=%u\n"
+		      "\tTPSUBFRAMES=%u\n"
+		      "\tAUDIOMUXVER=%u\n"
+		      "\tPROTECTION=%u\n"
+		      "\tANCILLARY_BITRATE=%u\n"
+		      "\tMETADATA_MODE=%u\n",
+		      aacEncoder_GetParam(aes->enc, AACENC_AOT),
+		      aacEncoder_GetParam(aes->enc, AACENC_BITRATE),
+		      aacEncoder_GetParam(aes->enc, AACENC_BITRATEMODE),
+		      aacEncoder_GetParam(aes->enc, AACENC_SAMPLERATE),
+		      aacEncoder_GetParam(aes->enc, AACENC_SBR_MODE),
+		      aacEncoder_GetParam(aes->enc, AACENC_GRANULE_LENGTH),
+		      aacEncoder_GetParam(aes->enc, AACENC_CHANNELMODE),
+		      aacEncoder_GetParam(aes->enc, AACENC_CHANNELORDER),
+		      aacEncoder_GetParam(aes->enc, AACENC_SBR_RATIO),
+		      aacEncoder_GetParam(aes->enc, AACENC_AFTERBURNER),
+		      aacEncoder_GetParam(aes->enc, AACENC_BANDWIDTH),
+		      aacEncoder_GetParam(aes->enc, AACENC_PEAK_BITRATE),
+		      aacEncoder_GetParam(aes->enc, AACENC_TRANSMUX),
+		      aacEncoder_GetParam(aes->enc, AACENC_HEADER_PERIOD),
+		      aacEncoder_GetParam(aes->enc, AACENC_SIGNALING_MODE),
+		      aacEncoder_GetParam(aes->enc, AACENC_TPSUBFRAMES),
+		      aacEncoder_GetParam(aes->enc, AACENC_AUDIOMUXVER),
+		      aacEncoder_GetParam(aes->enc, AACENC_PROTECTION),
+		      aacEncoder_GetParam(aes->enc, AACENC_ANCILLARY_BITRATE),
+		      aacEncoder_GetParam(aes->enc, AACENC_METADATA_MODE));
+
+		/* aac_encode_fmtp(&prm); */
 	}
-
-	error |= aacEncoder_SetParam(aes->enc, AACENC_AOT, AOT_ER_AAC_LD);
-	error |= aacEncoder_SetParam(aes->enc, AACENC_TRANSMUX,
-				     TT_MP4_LATM_MCP1);
-	error |= aacEncoder_SetParam(aes->enc, AACENC_SAMPLERATE, ac->srate);
-	error |= aacEncoder_SetParam(aes->enc, AACENC_CHANNELMODE,
-				     AAC_CHANNELS);
-	error |= aacEncoder_SetParam(aes->enc, AACENC_GRANULE_LENGTH,
-				     FRAME_SIZE);
-	error |= aacEncoder_SetParam(aes->enc, AACENC_TPSUBFRAMES, 2);
-	error |= aacEncoder_SetParam(aes->enc, AACENC_BITRATE, AAC_BITRATE);
-	if (error != AACENC_OK) {
-		err = EINVAL;
-		goto out;
-	}
-
 	*aesp = aes;
 
- out:
+out:
 	if (err)
 		mem_deref(aes);
 
@@ -85,20 +267,23 @@ int aac_encode_update(struct auenc_state **aesp, const struct aucodec *ac,
 }
 
 
-int aac_encode_frm(struct auenc_state *aes,
-		   bool *marker, uint8_t *buf, size_t *len,
-		   int fmt, const void *sampv, size_t sampc)
+int aac_encode_frm(struct auenc_state *aes, bool *marker, uint8_t *buf,
+                   size_t *len, int fmt, const void *sampv, size_t sampc)
 {
+	/*warning("aac_encode_frm()\n");*/
 	AACENC_BufDesc in_buf, out_buf;
-	AACENC_InArgs  in_args;
+	AACENC_InArgs in_args;
 	AACENC_OutArgs out_args;
 	AACENC_ERROR error;
 	INT in_id = IN_AUDIO_DATA, in_size, in_elem_size = sizeof(int16_t);
 	INT out_id = OUT_BITSTREAM_DATA, out_size, out_elem_size = 1;
+
 	const int16_t *s16 = sampv;
 	size_t total = 0;
-	size_t i;
-	(void)marker;
+	size_t sampi = 0;
+
+	/* uint16_t au_sizes[UINT8_MAX]; */
+	uint8_t i = 0;
 
 	if (!aes || !buf || !len || !sampv)
 		return EINVAL;
@@ -106,41 +291,74 @@ int aac_encode_frm(struct auenc_state *aes,
 	if (fmt != AUFMT_S16LE)
 		return ENOTSUP;
 
-	for (i=0; i<sampc; i+=FRAME_SIZE) {
+	/*
+	 * ToDo: Implement encoding of multiple
+	 * access units. May need intermediate
+	 * buffering of au data before writing
+	 * headers and header length.
+	 */
 
-		in_size = sizeof(int16_t) * FRAME_SIZE;
+	buf += AU_HDR_LEN; /* single access unit only! */
 
-		in_buf.numBufs           = 1;
-		in_buf.bufs              = (void **)&s16;
+	while ((sampc > sampi) && (i < UINT8_MAX)) {
+		in_size = sizeof(int16_t) * (sampc - sampi);
+
+		in_buf.numBufs = 1;
+		in_buf.bufs = (void **)&s16;
 		in_buf.bufferIdentifiers = &in_id;
-		in_buf.bufSizes          = &in_size;
-		in_buf.bufElSizes        = &in_elem_size;
+		in_buf.bufSizes = &in_size;
+		in_buf.bufElSizes = &in_elem_size;
 
-		out_size = (INT)*len;
+		out_size = *len - total - AU_HDR_LEN; /* 1 au only! */
 
-		out_buf.numBufs           = 1;
-		out_buf.bufs              = (void **)&buf;
+		out_buf.numBufs = 1;
+		out_buf.bufs = (void **)&buf;
 		out_buf.bufferIdentifiers = &out_id;
-		out_buf.bufSizes          = &out_size;
-		out_buf.bufElSizes        = &out_elem_size;
+		out_buf.bufSizes = &out_size;
+		out_buf.bufElSizes = &out_elem_size;
 
-		in_args.numInSamples = FRAME_SIZE;
+		in_args.numInSamples = sampc - sampi;
 		in_args.numAncBytes = 0;
 
 		error = aacEncEncode(aes->enc, &in_buf, &out_buf, &in_args,
-				     &out_args);
+		                     &out_args);
 		if (error != AACENC_OK) {
-			warning("aac: Unable to encode frame: 0x%x\n", error);
+			warning("aac: aacEncEncode() failed (0x%x)\n",
+			        error);
 			return EINVAL;
 		}
 
-		s16 += FRAME_SIZE;
+		sampi += out_args.numInSamples;
+		s16 += out_args.numInSamples;
 
 		buf += out_args.numOutBytes;
 		total += out_args.numOutBytes;
+
+		if (out_args.numOutBytes > 0) {
+			/* au_sizes[i] = out_args.numOutBytes; */
+			if (i > 0)
+			 	/* single access unit only! */
+				warning("aac: Sorry, encoding multiple AU "
+				        "per packet is not implemented yet.\n"
+				        "Please reduce the amount of samples "
+				        "passed to encoder per packet by "
+				        "lowering ptime value.\n");
+			++i;
+		}
 	}
 
-	*len = total;
+	if (total == 0) {
+		*len = 0;
+		return 0;
+ 	}
+
+	*marker = true;
+
+	buf -= total + AU_HDR_LEN; /* single access unit only! */
+
+	hdr_encode(buf, total);
+
+	*len = total + AU_HDR_LEN; /* single access unit only! */
 
 	return 0;
 }
