c7a32bb53fd702517bbc7b33ef961012eb2597d1
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2020-03-29 18:03:36 +0200

    test: add mock aufilt decoder

--- test/mock/mock_aufilt.c
+++ test/mock/mock_aufilt.c
@@ -15,10 +15,22 @@ struct mock_enc {
 	struct aufilt_enc_st af;  /* inheritance */
 };
 
+struct mock_dec {
+	struct aufilt_enc_st af;  /* inheritance */
+};
+
 
 static void enc_destructor(void *arg)
 {
 	struct mock_enc *st = (struct mock_enc *)arg;
+
+	list_unlink(&st->af.le);
+}
+
+
+static void dec_destructor(void *arg)
+{
+	struct mock_dec *st = (struct mock_dec *)arg;
 
 	list_unlink(&st->af.le);
 }
@@ -35,6 +47,11 @@ static int mock_encode_update(struct aufilt_enc_st **stp, void **ctx,
 	if (!stp || !af || !prm)
 		return EINVAL;
 
+	if (prm->srate==0 || prm->ch==0 || aufmt_sample_size(prm->fmt)==0) {
+		warning("mock_aufilt: enc: invalid srate/ch/fmt params\n");
+		return EINVAL;
+	}
+
 	if (*stp)
 		return 0;
 
@@ -48,17 +65,58 @@ static int mock_encode_update(struct aufilt_enc_st **stp, void **ctx,
 }
 
 
+static int mock_decode_update(struct aufilt_dec_st **stp, void **ctx,
+			      const struct aufilt *af, struct aufilt_prm *prm,
+			      const struct audio *au)
+{
+	struct mock_dec *st;
+	(void)ctx;
+	(void)au;
+
+	if (!stp || !af || !prm)
+		return EINVAL;
+
+	if (prm->srate==0 || prm->ch==0 || aufmt_sample_size(prm->fmt)==0) {
+		warning("mock_aufilt: dec: invalid srate/ch/fmt params\n");
+		return EINVAL;
+	}
+
+	if (*stp)
+		return 0;
+
+	st = (struct mock_dec *)mem_zalloc(sizeof(*st), dec_destructor);
+	if (!st)
+		return ENOMEM;
+
+	*stp = (struct aufilt_dec_st *)st;
+
+	return 0;
+}
+
+
 static int mock_encode(struct aufilt_enc_st *st, struct auframe *af)
 {
 	if (!st || !af)
 		return EINVAL;
 
-	/* check that sampv is ref'able */
-	if (0 == mem_nrefs(af->sampv))
-		return EBADMSG;
+	if (0 == auframe_size(af) || !af->sampv) {
+		warning("mock_aufilt: encode: invalid auframe\n");
+		return EINVAL;
+	}
 
-	if (0 == auframe_size(af))
-		return EBADMSG;
+	return 0;
+}
+
+
+static int mock_decode(struct aufilt_dec_st *st, struct auframe *af)
+{
+	if (!st || !af)
+		return EINVAL;
+
+	if (0 == auframe_size(af) || !af->sampv) {
+		warning("mock_aufilt: decode: invalid auframe\n");
+		return EINVAL;
+	}
 
 	return 0;
 }
@@ -68,6 +126,8 @@ static struct aufilt af_dummy = {
 	.name    = "MOCK-AUFILT",
 	.encupdh = mock_encode_update,
 	.ench    = mock_encode,
+	.decupdh = mock_decode_update,
+	.dech    = mock_decode,
 };
 
 
