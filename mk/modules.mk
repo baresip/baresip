7d8c46342888ed9e046d578fe2acd13e3c9f3a9d
Juha Heinanen <jh@tutpro.com>
2020-04-08 14:11:34 +0300

    modules.mk: added missing \ at and of line

--- mk/modules.mk
+++ mk/modules.mk
@@ -88,7 +88,7 @@ USE_AVFORMAT := $(shell ([ -f $(SYSROOT)/include/libavformat/avformat.h ] || \
 	[ -f $(SYSROOT_LOCAL)/include/libavformat/avformat.h ] || \
 	[ -f $(SYSROOT)/local/include/libavformat/avformat.h ] || \
 	[ -f $(SYSROOT)/include/$(MACHINE)/libavformat/avformat.h ] || \
-	[ -f $(SYSROOT_ALT)/include/libavformat/avformat.h ]) &&
+	[ -f $(SYSROOT_ALT)/include/libavformat/avformat.h ]) && \
 	([ -f $(SYSROOT)/include/libavformat/avdevice.h ] || \
 	[ -f $(SYSROOT_LOCAL)/include/libavdevice/avdevice.h ] || \
 	[ -f $(SYSROOT)/local/include/libavdevice/avdevice.h ] || \
