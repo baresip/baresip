e3645644fd21add56a8c7a55ce516263fc8ce8ca
Juha Heinanen <jh@tutpro.com>
2020-12-25 16:42:13 +0200

    Reordered video_stop_display

--- include/baresip.h
+++ include/baresip.h
@@ -1252,8 +1252,8 @@ int  video_encoder_set(struct video *v, struct vidcodec *vc,
 int  video_update(struct video *v, struct media_ctx **ctx, const char *peer);
 int  video_start_source(struct video *v, struct media_ctx **ctx);
 int  video_start_display(struct video *v, const char *peer);
-void video_stop(struct video *v);
 void video_stop_display(struct video *v);
+void video_stop(struct video *v);
 int   video_set_fullscreen(struct video *v, bool fs);
 void  video_vidsrc_set_device(struct video *v, const char *dev);
 int   video_set_source(struct video *v, const char *name, const char *dev);
