211499838ec6d4ce10d2329964a1909de7ae65eb
Juha Heinanen <jh@tutpro.com>
2020-12-30 15:14:45 +0200

    Supressed clang zero length array warning

--- modules/zrtp/module.mk
+++ modules/zrtp/module.mk
@@ -20,6 +20,6 @@ MOD		:= zrtp
 $(MOD)_SRCS	+= zrtp.c
 $(MOD)_LFLAGS	+= -lzrtp -lbn
 $(MOD)_CFLAGS   += -isystem /usr/local/include/libzrtp
-$(MOD)_CFLAGS   += -Wno-strict-prototypes
+$(MOD)_CFLAGS   += -Wno-strict-prototypes -Wno-zero-length-array
 
 include mk/mod.mk
