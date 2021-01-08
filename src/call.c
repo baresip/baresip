57229cf44c6bb9c0760f3a9e609ee39ce1773ebe
Christian Spielberger <c.spielberger@commend.com>
2021-01-08 10:05:06 +0100

    call: SIP auto answer - decode RFC 5373 answer mode

--- src/call.c
+++ src/call.c
@@ -37,6 +37,7 @@ struct call {
 	struct video *video;      /**< Video stream                         */
 	struct media_ctx *ctx;    /**< Shared A/V source media context      */
 	enum call_state state;    /**< Call state                           */
+	int32_t adelay;           /**< Auto answer delay in ms              */
 	char *local_uri;          /**< Local SIP uri                        */
 	char *local_name;         /**< Local display name                   */
 	char *peer_uri;           /**< Peer SIP Address                     */
@@ -613,6 +614,52 @@ static int assign_linenum(uint32_t *linenum, const struct list *lst)
 
 
 /**
+ * Decode the SIP-Header for RFC 5373 auto answer of incoming call
+ *
+ * @param call Call object
+ * @param msg  SIP message
+ * @param name SIP header name
+ */
+static void call_rfc5373_autoanswer(struct call *call,
+		const struct sip_msg *msg, const char *name)
+{
+	const struct sip_hdr *hdr;
+	struct pl v1;
+
+	hdr = sip_msg_xhdr(msg, name);
+	if (!hdr || pl_strcasecmp(&hdr->val, "Auto"))
+		return;
+
+	if (!msg_param_exists(&hdr->val, "require", &v1) &&
+			!account_sip_autoanswer(call->acc)) {
+
+		warning("call: rejected, since %s is not allowed\n", name);
+		call_hangup(call, 0, NULL);
+		return;
+	}
+
+	call->adelay = 0;
+}
+
+
+/**
+ * Decode the SIP-Header for auto answer options of incoming call
+ *
+ * @param call Call object
+ * @param msg  SIP message
+ */
+static void call_decode_sip_autoanswer(struct call *call,
+		const struct sip_msg *msg)
+{
+	call->adelay = -1;
+
+	/* RFC 5373 */
+	call_rfc5373_autoanswer(call, msg, "Answer-Mode");
+	call_rfc5373_autoanswer(call, msg, "Priv-Answer-Mode");
+}
+
+
+/**
  * Allocate a new Call state object
  *
  * @param callp       Pointer to allocated Call state object
@@ -675,6 +722,7 @@ int call_alloc(struct call **callp, const struct config *cfg, struct list *lst,
 	call->eh     = eh;
 	call->arg    = arg;
 	call->af     = prm->af;
+	call_decode_sip_autoanswer(call, msg);
 
 	err = str_dup(&call->local_uri, local_uri);
 	if (local_name)
@@ -759,7 +807,7 @@ int call_alloc(struct call **callp, const struct config *cfg, struct list *lst,
 				  video_error_handler, call);
 		if (err)
 			goto out;
- 	}
+	}
 
 	/* inherit certain properties from original call */
 	if (xcall) {
@@ -1215,10 +1263,12 @@ int call_debug(struct re_printf *pf, const struct call *call)
 	err |= re_hprintf(pf,
 			  " local_uri: %s <%s>\n"
 			  " peer_uri:  %s <%s>\n"
-			  " af=%s id=%s\n",
+			  " af=%s id=%s\n"
+			  " autoanswer delay: %d\n",
 			  call->local_name, call->local_uri,
 			  call->peer_name, call->peer_uri,
-			  net_af2name(call->af), call->id);
+			  net_af2name(call->af), call->id,
+			  call->adelay);
 	err |= re_hprintf(pf, " direction: %s\n",
 			  call->outgoing ? "Outgoing" : "Incoming");
 
@@ -2253,6 +2303,17 @@ void call_enable_rtp_timeout(struct call *call, uint32_t timeout_ms)
 uint32_t call_linenum(const struct call *call)
 {
 	return call ? call->linenum : 0;
+}
+
+
+/**
+ * Get the answer delay of this call
+ *
+ * @return answer delay in ms
+ */
+int32_t call_answer_delay(const struct call *call)
+{
+	return call ? call->adelay : -1;
 }
 
 
