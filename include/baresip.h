9173260022035a39608062537de0a11604212d60
Christian Spielberger <c.spielberger@commend.com>
2021-01-20 08:43:48 +0100

    ua: improve UA selection for incoming calls
    This allows to deny certain transport protocols (e.g. only allow TLS) for
    incoming peer-to-peer calls. Also checks for correct address family which is
    important if there is no af hint in the SDP.

--- include/baresip.h
+++ include/baresip.h
@@ -835,6 +835,7 @@ int  uag_reset_transp(bool reg, bool reinvite);
 void uag_set_sub_handler(sip_msg_h *subh);
 int  uag_set_extra_params(const char *eprm);
 struct ua   *uag_find(const struct pl *cuser);
+struct ua   *uag_find_msg(const struct sip_msg *msg);
 struct ua   *uag_find_aor(const char *aor);
 struct ua   *uag_find_param(const char *name, const char *val);
 struct sip  *uag_sip(void);
