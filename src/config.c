f6736fdd354932de83cb92e1a22acbd98533bf17
Andrew Webster <webstean@gmail.com>
2021-03-08 09:34:41 +1100

    removed newline

--- src/config.c
+++ src/config.c
@@ -1078,8 +1078,7 @@ int config_write_template(const char *file, const struct config *cfg)
 			 "\n# multicast receivers (in priority order)"
 			 "- port number must be even\n"
                          "#multicast_listener\t\t224.0.2.21:50000\n"
-			 "#multicast_listener\t\t224.0.2.21:50002\n"
-			 "\n");
+			 "#multicast_listener\t\t224.0.2.21:50002\n");
 	if (f)
 		(void)fclose(f);
 
