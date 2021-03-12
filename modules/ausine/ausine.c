6ec70852e5c3ac5e90e42f71850e3479a222de4c
Sebastian Reimers <hallo@studio-link.de>
2021-03-12 15:03:24 +0100

    ausine: mono support and stereo_left/right option

--- modules/ausine/ausine.c
+++ modules/ausine/ausine.c
@@ -23,10 +23,19 @@
  *
  \verbatim
   audio_source            ausine,400
+  audio_source            ausine,400,stereo_left
+  audio_source            ausine,400,stereo_right
  \endverbatim
  */
 
 static const double PI = 3.14159265358979323846264338328;
+
+enum channels {
+	STEREO,       /* Default 2ch (detected) */
+	STEREO_LEFT,  /* Stereo left channel only (manual) */
+	STEREO_RIGHT, /* Stereo right channel only (manual) */
+	MONO          /* Fallback 1ch (detected) */
+};
 
 struct ausrc_st {
 	const struct ausrc *as;  /* base class */
@@ -40,6 +49,7 @@ struct ausrc_st {
 	void *arg;
 	int freq;
 	double sec_offset;
+	enum channels ch;
 };
 
 
@@ -66,6 +76,7 @@ static void *play_thread(void *arg)
 	double sec_per_frame = 1.0 / 48000;
 	int inc;
 	size_t frames;
+	int16_t f;
 
 	sampv = mem_alloc(st->sampc * sizeof(int16_t), NULL);
 	if (!sampv)
@@ -90,14 +101,39 @@ static void *play_thread(void *arg)
 
 		inc = 0;
 		rad_per_sec = st->freq * 2.0 * PI;
-		frames = st->sampc / 2;
+
+		if (st->ch == MONO) {
+			frames = st->sampc;
+		}
+		else {
+			frames = st->sampc / 2;
+		}
 
 		for (frame = 0; frame < frames; frame += 1) {
 			sample = sin((st->sec_offset + frame * sec_per_frame)
 					* rad_per_sec);
-			sampv[inc] = (int16_t)(SCALE * 50 / 100.0f * sample);
-			sampv[inc+1] = (int16_t)(SCALE * 50 / 100.0f * sample);
-			inc += 2;
+
+			f = (int16_t)(SCALE * 50 / 100.0f * sample);
+
+			if (st->ch == STEREO) {
+				sampv[inc] = f;
+				sampv[inc + 1] = f;
+				inc += 2;
+			}
+			if (st->ch == STEREO_LEFT) {
+				sampv[inc] = f;
+				sampv[inc + 1] = 0;
+				inc += 2;
+			}
+			if (st->ch == STEREO_RIGHT) {
+				sampv[inc] = 0;
+				sampv[inc + 1] = f;
+				inc += 2;
+			}
+			if (st->ch == MONO) {
+				sampv[inc] = f;
+				inc += 1;
+			}
 		}
 
 		st->sec_offset = fmod(st->sec_offset + sec_per_frame * frames,
@@ -111,6 +147,22 @@ static void *play_thread(void *arg)
 	mem_deref(sampv);
 
 	return NULL;
+}
+
+
+static enum channels stereo_conf(const char *dev) {
+	struct pl r, pl1, pl2 = pl_null;
+
+	pl_set_str(&r, dev);
+	re_regex(r.p, r.l, "[^,]+,[~]*", &pl1, &pl2);
+
+	if (pl_isset(&pl2)) {
+		if (!pl_strcmp(&pl2, "stereo_left"))
+			return STEREO_LEFT;
+		if (!pl_strcmp(&pl2, "stereo_right"))
+			return STEREO_RIGHT;
+	}
+	return STEREO;
 }
 
 
@@ -137,10 +189,6 @@ static int alloc_handler(struct ausrc_st **stp, const struct ausrc *as,
 		return ENOTSUP;
 	}
 
-	if (prm->ch != 2) {
-		warning("ausine: supports only 2 channels");
-		return ENOTSUP;
-	}
 
 	st = mem_zalloc(sizeof(*st), destructor);
 	if (!st)
@@ -154,6 +202,11 @@ static int alloc_handler(struct ausrc_st **stp, const struct ausrc *as,
 
 	st->freq = atoi(dev);
 
+	st->ch = stereo_conf(dev);
+	if (prm->ch == 1) {
+		st->ch = MONO;
+	}
+
 	if (st->freq < 10 || st->freq > 20000)
 	{
 		warning("ausine: frequency must be between 10 and 20000 Hz\n");
@@ -161,8 +214,8 @@ static int alloc_handler(struct ausrc_st **stp, const struct ausrc *as,
 		goto out;
 	}
 
-	info("ausine: %u Hz, %d channels, frequency %d Hz\n",
-					prm->srate, prm->ch, st->freq);
+	info("ausine: %u Hz, %d channels, frequency %d Hz CH_MODE: %d\n",
+					prm->srate, prm->ch, st->freq, st->ch);
 
 	st->sampc = prm->srate * prm->ch * prm->ptime / 1000;
 
