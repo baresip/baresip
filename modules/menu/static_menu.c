7181268d559ee5b487ac509a2b07d09a126606c8
Christian Spielberger <c.spielberger@commend.com>
2021-03-15 16:34:07 +0100

    menu: remove call resume for command hangup (#1289)
    This feature was already removed for the CALL_CLOSED event.

--- modules/menu/static_menu.c
+++ modules/menu/static_menu.c
@@ -630,7 +630,6 @@ static int cmd_hangup(struct re_printf *pf, void *arg)
 	const struct cmd_arg *carg = arg;
 	struct ua *ua = carg->data ? carg->data : menu_uacur();
 	struct call *call = ua_call(ua);
-	bool resume;
 
 	(void)pf;
 
@@ -649,12 +648,7 @@ static int cmd_hangup(struct re_printf *pf, void *arg)
 		return ENOENT;
 	}
 
-	resume = call_state(call) == CALL_STATE_ESTABLISHED &&
-		 !call_is_onhold(call);
 	ua_hangup(ua, call, 0, NULL);
-
-	if (resume)
-		uag_hold_resume(NULL);
 
 	return 0;
 }
