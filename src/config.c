05784e01df7316013a81ca7b1ded7623d30de5cf
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2021-04-09 17:12:26 +0200

    config: disable ctrl_dbus in config template

--- src/config.c
+++ src/config.c
@@ -972,7 +972,7 @@ int config_write_template(const char *file, const struct config *cfg)
 	(void)re_fprintf(f, "#module_app\t\t" "mqtt" MOD_EXT "\n");
 	(void)re_fprintf(f, "#module_app\t\t" "ctrl_tcp" MOD_EXT "\n");
 	(void)re_fprintf(f, "module_app\t\t" "vidloop"MOD_EXT"\n");
-	(void)re_fprintf(f, "module_app\t\t" "ctrl_dbus"MOD_EXT"\n");
+	(void)re_fprintf(f, "#module_app\t\t" "ctrl_dbus"MOD_EXT"\n");
 	(void)re_fprintf(f, "#module_app\t\t" "httpreq"MOD_EXT"\n");
 	(void)re_fprintf(f, "#module_app\t\t" "multicast"MOD_EXT"\n");
 	(void)re_fprintf(f, "\n");
