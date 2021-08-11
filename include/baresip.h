4ae743e994e8f1f87fc98f08850af4c12e4cb980
Sebastian Reimers <hallo@studio-link.de>
2021-08-11 13:45:34 +0200

    core: move stream_enable_rtp_timeout to api (#1569)

--- include/baresip.h
+++ include/baresip.h
@@ -1373,6 +1373,7 @@ void stream_set_session_handlers(struct stream *strm,
 				 stream_error_h *errorh, void *arg);
 const char *stream_name(const struct stream *strm);
 int  stream_debug(struct re_printf *pf, const struct stream *s);
+void stream_enable_rtp_timeout(struct stream *strm, uint32_t timeout_ms);
 
 
 /*
