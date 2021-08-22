38df54bc504f228bdcbd255c23b72d08860f3ed6
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2021-08-22 09:46:24 +0200

    audio: remove printing of ignored RTP header extension

--- src/audio.c
+++ src/audio.c
@@ -1142,10 +1142,6 @@ static void stream_recv_handler(const struct rtp_header *hdr,
 			a->rx.level_last = -(double)(extv[i].data[0] & 0x7f);
 			a->rx.level_set = true;
 		}
-		else {
-			debug("audio: rtp header ext ignored (id=%u)\n",
-			     extv[i].id);
-		}
 	}
 
 	/* Save timestamp for incoming RTP packets */
