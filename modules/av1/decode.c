f78e1788835802ccded0cf6bf9ee5eb12f7f78e4
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2021-12-11 20:12:05 +0100

    av1: disable decode log

--- modules/av1/decode.c
+++ modules/av1/decode.c
@@ -163,7 +163,7 @@ int av1_decode(struct viddec_state *vds, struct vidframe *frame,
 	if (err)
 		return err;
 
-#if 1
+#if 0
 	debug("av1: decode: header:  [%s]  z=%u  y=%u  w=%u  n=%u\n",
 	      marker ? "M" : " ",
 	      hdr.z, hdr.y, hdr.w, hdr.n);
