e0312b55eb5e1799a7d01ba0b86bb265b979bac3
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2022-01-28 14:54:19 +0100

    config: remove old module rst.so from template

--- src/config.c
+++ src/config.c
@@ -1013,7 +1013,6 @@ int config_write_template(const char *file, const struct config *cfg)
 
 	(void)re_fprintf(f, "\n# Audio/Video source modules\n");
 	(void)re_fprintf(f, "#module\t\t\t" "avformat" MOD_EXT "\n");
-	(void)re_fprintf(f, "#module\t\t\t" "rst" MOD_EXT "\n");
 	(void)re_fprintf(f, "#module\t\t\t" "gst" MOD_EXT "\n");
 	(void)re_fprintf(f, "#module\t\t\t" "gst_video" MOD_EXT "\n");
 
