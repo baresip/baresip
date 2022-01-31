993b6bb47cac5912f639bda75afbbc3cfa4bdba0
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2022-01-31 10:50:34 +0100

    v4l2: remove deprecated format RGB565

--- modules/v4l2/v4l2.c
+++ modules/v4l2/v4l2.c
@@ -73,8 +73,6 @@ static enum vidfmt match_fmt(u_int32_t fmt)
 	case V4L2_PIX_FMT_YUYV:   return VID_FMT_YUYV422;
 	case V4L2_PIX_FMT_UYVY:   return VID_FMT_UYVY422;
 	case V4L2_PIX_FMT_RGB32:  return VID_FMT_RGB32;
-	case V4L2_PIX_FMT_RGB565: return VID_FMT_RGB565;
-	case V4L2_PIX_FMT_RGB555: return VID_FMT_RGB555;
 	case V4L2_PIX_FMT_NV12:   return VID_FMT_NV12;
 	case V4L2_PIX_FMT_NV21:   return VID_FMT_NV21;
 	default:                  return VID_FMT_N;
