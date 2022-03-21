5478dcc40b90f6d1a4fd9f343a67e8d10429339c
Christian Spielberger <c.spielberger@commend.com>
2022-03-21 10:44:50 +0100

    commend-build.sh: add initial build script for Commend

---
+++ commend-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+echo "# DEPRECATED: This script was moved to ..."
+
+echo ""
+echo "git clone ssh://git@source.commend.com/sip/commend-tools.git"
+echo "./commend-tools/scripts/commend-baresip-build.sh"
+
+echo ""
+echo "#  Keep commend-baresip-build.sh up to date !"
+echo ""
