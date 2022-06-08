358eed8c7411defcd5cb3de5f5c28eebf2b74370
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2022-06-08 08:18:47 +0200

    Cmake findre (#1893)

---
+++ cmake/FindRE.cmake
@@ -0,0 +1,16 @@
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_LIBRE QUIET libre)
+
+find_path(RE_INCLUDE_DIR re.h
+  HINTS ../re/include ${PC_LIBRE_INCLUDEDIR} ${PC_LIBRE_INCLUDE_DIRS})
+
+find_library(RE_LIBRARY NAMES re libre
+  HINTS ../re ../re/build ${PC_LIBRE_LIBDIR} ${PC_LIBRE_LIBRARY_DIRS})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(RE DEFAULT_MSG RE_LIBRARY RE_INCLUDE_DIR)
+
+mark_as_advanced(RE_INCLUDE_DIR RE_LIBRARY)
+
+set(RE_INCLUDE_DIRS ${RE_INCLUDE_DIR})
+set(RE_LIBRARIES ${RE_LIBRARY})
