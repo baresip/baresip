3454f66be45e6dfb7954cc5685e78c7de927e9a4
Christoph Huber <c.huber@commend.com>
2022-06-22 09:37:20 +0200

    cmake: add pulse and pulse_async module

---
+++ cmake/FindPULSE.cmake
@@ -0,0 +1,40 @@
+# Find the system's pulse includes and library
+#
+#  PULSE_INCLUDE_DIRS - where to find pulseaudio.h
+#  PULSE_LIBRARIES    - List of libraries when using pulseaudio
+#  PULSE_FOUND        - True if pulseaudio found
+
+if(NOT WIN32)
+    find_package(PkgConfig)
+    pkg_search_module(PULSE pulse)
+endif()
+
+find_path(PULSE_INCLUDE_DIR
+    NAMES pulse/pulseaudio.h
+    HINTS
+        "${PULSE_INCLUDE_DIRS}"
+        "${PULSE_HINTS}/include"
+    PATHS /usr/local/include /usr/include
+)
+
+find_library(PULSE_LIBRARY
+    NAME pulse
+    HINTS
+        "${PULSE_LIBRARY_DIRS}"
+        "${PULSE_HINTS}/lib"
+    PATHS /usr/loca/lib usr/lib
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(PULSE DEFAULT_MSG PULSE_LIBRARY
+    PULSE_INCLUDE_DIR)
+
+if(PULSE_FOUND)
+    set( PULSE_INCLUDE_DIRS ${PULSE_INCLUDE_DIR} )
+    set( PULSE_LIBRARIES ${PULSE_LIBRARY} )
+else()
+    set( PULSE_INCLUDE_DIRS )
+    set( PULSE_LIBRARIES )
+endif()
+
+mark_as_advanced( PULSE_LIBRARIES PULSE_INCLUDE_DIRS )
