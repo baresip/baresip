5c3370f07586fb866d7c500c01039133f203f5a1
Juha Heinanen <jh@tutpro.com>
2022-07-16 14:17:42 +0300

    Revert PR #1922

--- src/call.c
+++ src/call.c
@@ -2204,14 +2204,18 @@ static void sipsess_progr_handler(const struct sip_msg *msg, void *arg)
 		break;
 	}
 
-	if (media) {
+	call_stream_stop(call);
+
+	if (media)
+		call_stream_start(call, false);
+
+	if (media)
 		call_event_handler(call, CALL_EVENT_PROGRESS, call->peer_uri);
-		update_media(call);
-	}
-	else {
-		call_stream_stop(call);
+	else
 		call_event_handler(call, CALL_EVENT_RINGING, call->peer_uri);
-	}
+
+	if (media)
+		update_media(call);
 }
 
 
