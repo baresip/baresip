c2a85a74e8ba272c9c84c0b6a5b10cd70ab7726e
Christian Spielberger <c.spielberger@commend.com>
2022-07-28 11:02:01 +0200

    cmake: FindGIO.cmake like proposed in the cmake manual

---
+++ cmake/FindGIO.cmake
@@ -0,0 +1,48 @@
+find_package(PkgConfig QUIET)
+pkg_check_modules(GIO gio-unix-2.0)
+
+find_path(GIO_INCLUDE_DIR
+  NAMES gio/gio.h
+  HINTS
+    "${GIO_INCLUDE_DIRS}"
+    "${GIO_HINTS}/include/gio-lib-2.0"
+  PATHS /usr/local/include/glib-2.0 /usr/include/glib-2.0
+)
+
+find_library(GLIB_LIBRARY
+  NAMES glib-2.0
+  HINTS
+    "${GIO_LIBRARY_DIRS}"
+    "${GIO_HINTS}/lib"
+  PATHS /usr/local/lib /usr/lib
+)
+
+find_library(GIO_LIBRARY
+  NAMES gio-2.0
+  HINTS
+    "${GIO_LIBRARY_DIRS}"
+    "${GIO_HINTS}/lib"
+  PATHS /usr/local/lib /usr/lib
+)
+
+find_library(GOBJ_LIBRARY
+  NAMES gobject-2.0
+  HINTS
+    "${GIO_LIBRARY_DIRS}"
+    "${GIO_HINTS}/lib"
+  PATHS /usr/local/lib /usr/lib
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GIO DEFAULT_MSG
+  GLIB_LIBRARY GIO_LIBRARY GOBJ_LIBRARY GIO_INCLUDE_DIR)
+
+if(GIO_FOUND)
+  set( GIO_INCLUDE_DIRS ${GIO_INCLUDE_DIR} )
+  set( GIO_LIBRARIES ${GLIB_LIBRARY} ${GIO_LIBRARY} ${GOBJ_LIBRARY})
+else()
+  set( GIO_INCLUDE_DIRS )
+  set( GIO_LIBRARIES )
+endif()
+
+mark_as_advanced( GIO_LIBRARIES GIO_INCLUDE_DIRS )
--- cmake/FindGLIB.cmake
+++
@@ -1,4 +0,0 @@
-find_package(PkgConfig QUIET)
-pkg_check_modules(GLIB glib-2.0 gio-unix-2.0)
-
-mark_as_advanced( GLIB_LIBRARIES GLIB_INCLUDE_DIRS )
