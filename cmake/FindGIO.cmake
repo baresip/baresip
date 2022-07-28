3254ff105ce94c4dd244b65201b8e47c1eed2253
Christian Spielberger <c.spielberger@commend.com>
2022-07-28 11:50:33 +0200

    cmake: ctrl_dbus fixes

--- cmake/FindGIO.cmake
+++ cmake/FindGIO.cmake
@@ -4,15 +4,31 @@ pkg_check_modules(GIO gio-unix-2.0)
 find_path(GIO_INCLUDE_DIR
   NAMES gio/gio.h
   HINTS
-    "${GIO_INCLUDE_DIRS}"
+    ${GIO_INCLUDE_DIRS}
     "${GIO_HINTS}/include/gio-lib-2.0"
   PATHS /usr/local/include/glib-2.0 /usr/include/glib-2.0
+)
+
+find_path(GLIB_INCLUDE_DIR
+  NAMES glibconfig.h
+  HINTS
+    ${GIO_INCLUDE_DIRS}
+    "${GLIB_HINTS}/glib-2.0/include"
+  PATHS /usr/local/include/glib-2.0 /usr/include/glib-2.0
+)
+
+find_path(GIO_UNIX_INCLUDE_DIR
+  NAMES gio/gunixfdlist.h
+  HINTS
+    ${GIO_INCLUDE_DIRS}
+    "${GIO_HINTS}/include/gio-unix-2.0"
+  PATHS /usr/local/include/gio-unix-2.0 /usr/include/gio-unix-2.0
 )
 
 find_library(GLIB_LIBRARY
   NAMES glib-2.0
   HINTS
-    "${GIO_LIBRARY_DIRS}"
+    ${GIO_LIBRARY_DIRS}
     "${GIO_HINTS}/lib"
   PATHS /usr/local/lib /usr/lib
 )
@@ -20,7 +36,7 @@ find_library(GLIB_LIBRARY
 find_library(GIO_LIBRARY
   NAMES gio-2.0
   HINTS
-    "${GIO_LIBRARY_DIRS}"
+    ${GIO_LIBRARY_DIRS}
     "${GIO_HINTS}/lib"
   PATHS /usr/local/lib /usr/lib
 )
@@ -28,17 +44,19 @@ find_library(GIO_LIBRARY
 find_library(GOBJ_LIBRARY
   NAMES gobject-2.0
   HINTS
-    "${GIO_LIBRARY_DIRS}"
+    ${GIO_LIBRARY_DIRS}
     "${GIO_HINTS}/lib"
   PATHS /usr/local/lib /usr/lib
 )
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GIO DEFAULT_MSG
-  GLIB_LIBRARY GIO_LIBRARY GOBJ_LIBRARY GIO_INCLUDE_DIR)
+  GLIB_LIBRARY GIO_LIBRARY GOBJ_LIBRARY
+  GIO_INCLUDE_DIR GLIB_INCLUDE_DIR GIO_UNIX_INCLUDE_DIR)
 
 if(GIO_FOUND)
-  set( GIO_INCLUDE_DIRS ${GIO_INCLUDE_DIR} )
+  set( GIO_INCLUDE_DIRS ${GIO_INCLUDE_DIR} ${GLIB_INCLUDE_DIR}
+                        ${GIO_UNIX_INCLUDE_DIR} )
   set( GIO_LIBRARIES ${GLIB_LIBRARY} ${GIO_LIBRARY} ${GOBJ_LIBRARY} )
 else()
   set( GIO_INCLUDE_DIRS )
