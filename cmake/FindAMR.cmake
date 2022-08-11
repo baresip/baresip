664df1d42bf4fdb27f1b0a4a0bbc57e832953066
Juha Heinanen <jh@tutpro.com>
2022-08-11 18:52:46 +0300

    FindAMR.cmake fixes/improvements

--- cmake/FindAMR.cmake
+++ cmake/FindAMR.cmake
@@ -1,11 +1,5 @@
-
-if(NOT WIN32)
-  find_package(PkgConfig)
-  pkg_search_module(AMR opencore-amrwb)
-endif()
-
 find_path(AMR_INCLUDE_DIR
-  NAMES opencore-amrwb/dec_if.h
+  NAMES opencore-amrwb/dec_if.h opencore-amrnb/interf_enc.h
   HINTS
     "${AMR_INCLUDE_DIRS}"
     "${AMR_HINTS}/include"
@@ -13,11 +7,43 @@ find_path(AMR_INCLUDE_DIR
 )
 
 find_library(AMR_LIBRARY
-  NAMES opencore-amrwb
+  NAMES opencore-amrwb opencore-amrnb
   HINTS
     "${AMR_LIBRARY_DIRS}"
     "${AMR_HINTS}/lib"
   PATHS /usr/local/lib /usr/lib
+)
+
+find_path(NB_INCLUDE_DIR
+  NAME opencore-amrnb/interf_enc.h
+  HINTS
+    "${AMR_INCLUDE_DIRS}"
+    "${AMR_HINTS}/include"
+  PATHS /usr/local/include /usr/include
+)
+
+find_path(WB_INCLUDE_DIR
+  NAME opencore-amrwb/dec_if.h
+  HINTS
+    "${AMR_INCLUDE_DIRS}"
+    "${AMR_HINTS}/include"
+  PATHS /usr/local/include /usr/include
+)
+
+find_library(NB_LIBRARY
+  NAME opencore-amrnb
+  HINTS
+    "${AMR_LIBRARY_DIRS}"
+    "${AMR_HINTS}/lib"
+  PATHS /usr/local/lib usr/lib
+)
+
+find_library(WB_LIBRARY
+  NAME opencore-amrwb
+  HINTS
+    "${AMR_LIBRARY_DIRS}"
+    "${AMR_HINTS}/lib"
+  PATHS /usr/local/lib usr/lib
 )
 
 include(FindPackageHandleStandardArgs)
@@ -25,11 +51,19 @@ find_package_handle_standard_args(AMR DEFAULT_MSG AMR_LIBRARY
     AMR_INCLUDE_DIR)
 
 if(AMR_FOUND)
-  set( AMR_INCLUDE_DIRS ${AMR_INCLUDE_DIR} )
-  set( AMR_LIBRARIES ${AMR_LIBRARY} )
-else()
-  set( AMR_INCLUDE_DIRS )
-  set( AMR_LIBRARIES )
+  if ( NB_INCLUDE_DIR )
+    set( AMR_INCLUDE_DIRS ${NB_INCLUDE_DIR}/opencore-amrnb )
+    set( AMR_LIBRARIES ${NB_LIBRARY} )
+    set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DAMR_NB" )
+  else()
+    set( AMR_INCLUDE_DIRS "" )
+    set( AMR_LIBRARIES "" )
+  endif()
+  if ( WB_INCLUDE_DIR )
+    set( AMR_INCLUDE_DIRS ${AMR_INCLUDE_DIRS} ${WB_INCLUDE_DIR}/opencore-amrwb )
+    set( AMR_LIBRARIES ${AMR_LIBRARIES} ${WB_LIBRARY} )
+    set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DAMR_WB" )
+  endif()
 endif()
 
 mark_as_advanced( AMR_LIBRARIES AMR_INCLUDE_DIRS )
