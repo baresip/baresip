941947ca29cf8b752544b76497334573210c763f
Christoph Huber <c.huber@commend.com>
2022-08-23 14:39:43 +0200

    ctrl_tcp: change unsafe operations on an mbuf to the safe mbuf interface

--- modules/ctrl_tcp/tcp_netstring.c
+++ modules/ctrl_tcp/tcp_netstring.c
@@ -70,13 +70,22 @@ static bool netstring_send_handler(int *err, struct mbuf *mb, void *arg)
 	re_snprintf(num_str, sizeof(num_str), "%zu", mbuf_get_left(mb));
 	num_len = strlen(num_str);
 
-	mb->pos = NETSTRING_HEADER_SIZE - (num_len + 1);
-	mbuf_write_mem(mb, (uint8_t*) num_str, num_len);
-	mb->pos = NETSTRING_HEADER_SIZE - (num_len + 1);
-	mb->buf[mb->pos + num_len] = ':';
-	mb->buf[mb->end] = ',';
+	mbuf_set_pos(mb, NETSTRING_HEADER_SIZE - (num_len + 1));
+	*err = mbuf_write_mem(mb, (uint8_t*) num_str, num_len);
+	if (*err)
+		return true;
 
-	mb->end += 1;
+	mbuf_set_pos(mb, NETSTRING_HEADER_SIZE - (num_len + 1) + num_len);
+	*err = mbuf_write_u8(mb, ':');
+	if (*err)
+		return true;
+
+	mbuf_skip_to_end(mb);
+	*err = mbuf_write_u8(mb, ',');
+	if (*err)
+		return true;
+
+	mbuf_set_pos(mb, NETSTRING_HEADER_SIZE - (num_len + 1));
 
 	++netstring->n_tx;
 
