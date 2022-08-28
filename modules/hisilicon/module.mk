b00d120d699c87a72c79ab7801ccc98193ccc075
Dmitry Ilyin <dima@doty.ru>
2022-08-28 20:18:11 +0300

    Fix mk

--- modules/hisilicon/module.mk
+++ modules/hisilicon/module.mk
@@ -6,6 +6,7 @@
 
 MOD		:= hisilicon
 $(MOD)_SRCS	+= hisi.c hisi_src.c hisi_play.c
-$(MOD)_LFLAGS	+=
+$(MOD)_LFLAGS	+= -L$(HISI_SDK_DIR)/mpp/lib -lmpi -lVoiceEngine -lsecurec -lupvqe -ldnvqe
+$(MOD)_CFLAGS	+= -I-L$(HISI_SDK_DIR)/mpp/include
 
 include mk/mod.mk
