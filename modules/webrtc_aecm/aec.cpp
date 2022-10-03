ba1eba8688cdf312e3389aab5e4bc6aaf644e635
Juha Heinanen <jh@tutpro.com>
2022-10-03 15:42:35 +0300

    Avoid webrtc_aecm module C++20 extension warnings

--- modules/webrtc_aecm/aec.cpp
+++ modules/webrtc_aecm/aec.cpp
@@ -123,12 +123,12 @@ int webrtc_aecm_alloc(struct aec **stp, void **ctx, struct aufilt_prm *prm)
 
 
 static struct aufilt webrtc_aec = {
-	.le      = LE_INIT,
-	.name    = "webrtc_aecm",
-	.encupdh = webrtc_aecm_encode_update,
-	.ench    = webrtc_aecm_encode,
-	.decupdh = webrtc_aecm_decode_update,
-	.dech    = webrtc_aecm_decode
+	LE_INIT,
+	"webrtc_aecm",
+	webrtc_aecm_encode_update,
+	webrtc_aecm_encode,
+	webrtc_aecm_decode_update,
+	webrtc_aecm_decode
 };
 
 
