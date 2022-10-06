1248d6cc7a160ba3abfa4bf752bf1e526bedcfe5
Juha Heinanen <jh@tutpro.com>
2022-10-06 11:18:32 +0300

    FindPNG needs to find also include directory

--- cmake/FindPNG.cmake
+++ cmake/FindPNG.cmake
@@ -1,3 +1,11 @@
+find_path(PNG_INCLUDE_DIR
+  NAMES png.h
+  HINTS
+    "${PNG_INCLUDE_DIRS}"
+    "${PNG_HINTS}/include"
+  PATHS /usr/local/include /usr/include
+)
+
 find_library(PNG_LIBRARY
   NAME png
   HINTS
@@ -7,12 +15,15 @@ find_library(PNG_LIBRARY
 )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(PNG DEFAULT_MSG PNG_LIBRARY)
+find_package_handle_standard_args(PNG DEFAULT_MSG PNG_LIBRARY
+  PNG_INCLUDE_DIR)
 
 if(PNG_FOUND)
+  set( PNG_INCLUDE_DIRS ${PNG_INCLUDE_DIR} )
   set( PNG_LIBRARIES ${PNG_LIBRARY} )
 else()
+  set( PNG_INCLUDE_DIR )
   set( PNG_LIBRARIES )
 endif()
 
-mark_as_advanced( PNG_LIBRARIES )
+mark_as_advanced( PNG_INCLUDE_DIRS PNG_LIBRARIES )
