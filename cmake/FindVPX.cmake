9a134569198a2bf6d50472516e2bbce8f990b137
juha-h <jh@tutpro.com>
2022-10-06 12:53:15 +0300

    FindVPX needs to find also include directory (#2231)

--- cmake/FindVPX.cmake
+++ cmake/FindVPX.cmake
@@ -1,3 +1,11 @@
+find_path(VPX_INCLUDE_DIR
+  NAMES vpx/vpx_codec.h
+  HINTS
+    "${VPX_INCLUDE_DIRS}"
+    "${VPX_HINTS}/include"
+  PATHS /usr/local/include /usr/include
+)
+
 find_library(VPX_LIBRARY
   NAME vpx
   HINTS
@@ -7,12 +15,15 @@ find_library(VPX_LIBRARY
 )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(VPX DEFAULT_MSG VPX_LIBRARY)
+find_package_handle_standard_args(VPX DEFAULT_MSG VPX_LIBRARY
+  VPX_INCLUDE_DIR)
 
 if(VPX_FOUND)
+  set( VPX_INCLUDE_DIRS ${VPX_INCLUDE_DIR} )
   set( VPX_LIBRARIES ${VPX_LIBRARY} )
 else()
+  set( VPX_INCLUDE_DIRS )
   set( VPX_LIBRARIES )
 endif()
 
-mark_as_advanced( VPX_LIBRARIES )
+mark_as_advanced( VPX_INCLUDE_DIRS VPX_LIBRARIES )
