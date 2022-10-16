80ef35170507fac7e9f0f25f45a39e197c5e8416
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2022-10-16 09:25:33 +0200

    config: fix template for avcodec_xxx (#2258)

--- src/config.c
+++ src/config.c
@@ -1165,10 +1165,10 @@ int config_write_template(const char *file, const struct config *cfg)
 			"#avcodec_h264dec\th264\n"
 			"#avcodec_h265enc\tlibx265\n"
 			"#avcodec_h265dec\thevc\n"
-			"#avcodec_hwaccel\t%s\n",
-			default_avcodec_hwaccel(),
-			"#avcodec_profile_level_id 42002a\n",
-			"#avcodec_keyint\t\t10\n"
+			"#avcodec_hwaccel\t%s\n"
+			"#avcodec_profile_level_id 42002a\n"
+			"#avcodec_keyint\t\t10\n",
+			default_avcodec_hwaccel()
 			);
 
 	(void)re_fprintf(f,
