cedb99d6df5ffafe3245b441f68701f718f51205
Juha Heinanen <jh@tutpro.com>
2022-10-19 09:49:03 +0300

    Removed module avformat dependency on libpostproc

--- modules/avformat/module.mk
+++ modules/avformat/module.mk
@@ -10,6 +10,6 @@ $(MOD)_SRCS	+= audio.c
 $(MOD)_SRCS	+= video.c
 $(MOD)_LFLAGS	+= \
 	`pkg-config --libs libavformat libavcodec libswresample \
-		libavutil libavdevice libavfilter libswscale libpostproc`
+		libavutil libavdevice libavfilter libswscale`
 
 include mk/mod.mk
