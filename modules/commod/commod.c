fe1209db1d0a031ca7889b4edb6193be5c7dd9cf
Christian Spielberger <c.spielberger@commend.com>
2023-05-12 14:31:46 +0200

    SYMX-7747 commod: switch preview video command

--- modules/commod/commod.c
+++ modules/commod/commod.c
@@ -19,6 +19,22 @@
 #define DEBUG_LEVEL 5
 #include <re_dbg.h>
 
+struct amle {
+	struct le he;
+	struct account *acc;
+	enum answermode am;
+};
+
+struct commod {
+	struct play *cur_play;
+	struct call *cur_call;
+	struct hash *answmod;
+};
+
+
+static struct commod d = { NULL, NULL, NULL };
+
+
 /**
  * Print info for given call with Commend specific informations
  *
@@ -154,8 +170,6 @@ static int param_decode(const char *prm, const char *name, struct pl *val)
 const char *playmod_usage = "/com_playmod"
 			    " source=<audiofile>"
 			    " [player=<player_mod>,<player_dev>]\n";
-static struct play *cur_play = NULL;
-
 
 static int cmd_playmod_file(struct re_printf *pf, void *arg)
 {
@@ -178,7 +192,7 @@ static int cmd_playmod_file(struct re_printf *pf, void *arg)
 	cfg = conf_config();
 
 	/* Stop the current tone, if any */
-	cur_play = mem_deref(cur_play);
+	d.cur_play = mem_deref(d.cur_play);
 
 	if (param_decode(carg->prm, "source", &src_param)) {
 		re_hprintf(pf, "commod: No source defined.\n");
@@ -209,7 +223,7 @@ static int cmd_playmod_file(struct re_printf *pf, void *arg)
 		re_hprintf(pf, "playing audio file \"%s\" ..\n", filename);
 
 		err = play_file(
-			&cur_play, baresip_player(),
+			&d.cur_play, baresip_player(),
 			filename, 0,  alert_mod, alert_dev);
 
 		if (err) {
@@ -304,25 +318,175 @@ static void hangup_outgoing_ua(struct call *call, void *arg)
 }
 
 
+static int acc_add_answmod(struct account *acc)
+{
+	struct amle *amle;
+
+	amle = mem_zalloc(sizeof(*amle), NULL);
+	if (!amle)
+		return ENOMEM;
+
+	amle->am  = account_answermode(acc);
+	amle->acc = acc;
+	hash_append(d.answmod, hash_fast_str(account_aor(acc)),
+		    &amle->he, amle);
+	return 0;
+}
+
+
+static bool amle_restore_applyh(struct le *le, void *arg)
+{
+	struct amle *amle = le->data;
+	(void)arg;
+
+	account_set_answermode(amle->acc, amle->am);
+	return true;
+}
+
+
+static void acc_restore_answmods(void)
+{
+	hash_apply(d.answmod, amle_restore_applyh, NULL);
+	hash_flush(d.answmod);
+}
+
+
+static bool amle_get_applyh(struct le *le, void *arg)
+{
+	struct amle *amle = le->data;
+	enum answermode *amp = arg;
+
+	*amp = amle->am;
+	return true;
+}
+
+
+static enum answermode acc_answmod_get(const struct account *acc)
+{
+	enum answermode am = ANSWERMODE_MANUAL;
+
+	hash_lookup(d.answmod, hash_fast_str(account_aor(acc)),
+		    amle_get_applyh, &am);
+
+	return am;
+}
+
+
+static void sel_oldest_call(struct call *call, void *arg)
+{
+	struct call *closed = arg;
+
+	if (call == closed)
+		return;
+
+	if (call_state(d.cur_call) != CALL_STATE_ESTABLISHED &&
+	    call_state(call)       == CALL_STATE_ESTABLISHED)
+		d.cur_call = call;
+
+	else if (!d.cur_call && call_state(call) == CALL_STATE_INCOMING)
+		d.cur_call = call;
+}
+
+
+static void call_earlymedia_disable(struct call *call)
+{
+	if (call_refresh_allowed(call)) {
+		call_set_audio_ldir(call, SDP_INACTIVE);
+		call_set_video_ldir(call, SDP_INACTIVE);
+		call_modify(call);
+	}
+}
+
+
+static void call_earlymedia_enable(struct call *call)
+{
+	enum answermode am = acc_answmod_get(call_account(call));
+	enum sdp_dir adir = am == ANSWERMODE_EARLY ? SDP_SENDRECV :
+			    am == ANSWERMODE_EARLY_AUDIO ? SDP_RECVONLY :
+			    SDP_INACTIVE;
+	enum sdp_dir vdir = am == ANSWERMODE_EARLY ? SDP_SENDRECV :
+			    am == ANSWERMODE_EARLY_VIDEO ? SDP_RECVONLY :
+			    SDP_INACTIVE;
+
+	if (adir == SDP_INACTIVE && vdir == SDP_INACTIVE)
+		return;
+
+	if (call_refresh_allowed(call)) {
+		call_set_audio_ldir(call, adir);
+		call_set_video_ldir(call, vdir);
+		call_modify(call);
+	}
+	else {
+		call_progress_dir(call, adir, vdir);
+	}
+}
+
+
 static void ua_event_handler(struct ua *ua, enum ua_event ev,
 			     struct call *call, const char *prm, void *arg)
 {
 	struct account *acc = ua_account(ua);
+	enum answermode am = account_answermode(acc);
+	enum sdp_dir adir = sdp_media_rdir(stream_sdpmedia(audio_strm(
+					   call_audio(call))));
+	enum sdp_dir vdir = sdp_media_rdir(stream_sdpmedia(video_strm(
+					   call_video(call))));
+	bool video;
+	bool audio;
+	bool control;
 	(void) arg;
 
-	info("menu: [ ua=%s call=%s ] event: %s (%s)\n",
+	info("commod: [ ua=%s call=%s ] event: %s (%s)\n",
 	     account_aor(acc), call_id(call), uag_event_str(ev), prm);
 
+	video = (am == ANSWERMODE_EARLY || am == ANSWERMODE_EARLY_VIDEO) &&
+		vdir & SDP_RECVONLY;
+	audio = (am == ANSWERMODE_EARLY || am == ANSWERMODE_EARLY_AUDIO) &&
+		adir & SDP_RECVONLY;
+	control = video || audio;
 	switch (ev) {
 		case UA_EVENT_CALL_INCOMING:
 			check_auto_answer_media_direction(call);
+			if (control && !d.cur_call) {
+				d.cur_call = call;
+			}
+
+			if (d.cur_call != call) {
+				if (am != ANSWERMODE_MANUAL)
+					acc_add_answmod(acc);
+
+				account_set_answermode(acc, ANSWERMODE_MANUAL);
+			}
+
 			/*@fallthrough@*/
 		case UA_EVENT_CALL_OUTGOING:
-			cur_play = mem_deref(cur_play);
+			d.cur_play = mem_deref(d.cur_play);
 			break;
 		case UA_EVENT_REGISTER_FAIL:
 			/* SYFU-942: hangup all call-requests */
 			uag_filter_calls(hangup_outgoing_ua, NULL, ua);
+			break;
+		case UA_EVENT_CALL_ESTABLISHED:
+		case UA_EVENT_CALL_ANSWERED:
+			if (call_is_outgoing(call))
+			    break;
+
+			if (d.cur_call != call &&
+			    call_state(d.cur_call) == CALL_STATE_INCOMING) {
+				call_set_video_dir(d.cur_call, SDP_INACTIVE);
+			}
+
+			d.cur_call = call;
+			break;
+		case UA_EVENT_CALL_CLOSED:
+			if (d.cur_call == call) {
+				d.cur_call = NULL;
+				uag_filter_calls(sel_oldest_call, NULL, call);
+				if (d.cur_call)
+					call_earlymedia_enable(d.cur_call);
+				else
+					acc_restore_answmods();
+			}
 			break;
 		default:
 			break;
@@ -381,12 +545,63 @@ static int com_rm_aucodec(struct re_printf *pf, void *arg)
 }
 
 
+/**
+ * Switch early media to given incoming call
+ *
+ * @param pf		Print handler for debug output
+ * @param unused	unused parameter
+ *
+ * @return	0 if success, otherwise errorcode
+ */
+static int com_switch_earlymedia(struct re_printf *pf, void *arg)
+{
+	const struct cmd_arg *carg = arg;
+
+	struct call *call;
+	const char *usage = "usage: /com_switchearly <callid>\n";
+
+	if (!str_isset(carg->prm)) {
+		(void) re_hprintf(pf, "%s", usage);
+		return EINVAL;
+	}
+
+	if (!d.cur_call || call_state(d.cur_call) != CALL_STATE_INCOMING) {
+		(void) re_hprintf(pf, "No incoming call or "
+				  "established call\n");
+		return EINVAL;
+	}
+
+	call = uag_call_find(carg->prm);
+	if (!call) {
+		(void) re_hprintf(pf, "Could not find call %s\n", carg->prm);
+		return EINVAL;
+	}
+
+	if (call == d.cur_call)
+		return 0;
+
+	if (call_state(call) != CALL_STATE_INCOMING) {
+		(void) re_hprintf(pf, "Call %s has state %s\n",
+				  carg->prm, call_statename(call));
+		return EINVAL;
+	}
+
+	call_earlymedia_disable(d.cur_call);
+	call_earlymedia_enable(call);
+
+	d.cur_call = call;
+	return 0;
+}
+
+
 static const struct cmd cmdv[] = {
 
 {"com_listcalls", 0, 0,	"List active calls Commend format", com_print_calls},
 {"com_playmod",   0, CMD_PRM,	"Play audio file on audio player",
 	cmd_playmod_file},
 {"com_rmaucodec", 0, 0, "Remove current audio codec", com_rm_aucodec},
+{"com_switchearly", 0, 0, "Switch early media to other incoming call",
+	com_switch_earlymedia},
 };
 
 
@@ -396,6 +611,7 @@ static int module_init(void)
 
 	err = uag_event_register(ua_event_handler, NULL);
 	err |= cmd_register(baresip_commands(), cmdv, ARRAY_SIZE(cmdv));
+	err |= hash_alloc(&d.answmod, 32);
 
 	return err;
 }
@@ -405,6 +621,8 @@ static int module_close(void)
 {
 	uag_event_unregister(ua_event_handler);
 	cmd_unregister(baresip_commands(), cmdv);
+	hash_flush(d.answmod);
+	mem_deref(d.answmod);
 
 	return 0;
 }
