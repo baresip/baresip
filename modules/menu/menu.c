b954bcc90507f52fc9659c9afd0a512a7d1d69e7
Juha Heinanen <jh@tutpro.com>
2023-06-06 07:45:27 +0300

    Fixed lint warning

--- modules/menu/menu.c
+++ modules/menu/menu.c
@@ -797,7 +797,8 @@ static void ua_event_handler(struct ua *ua, enum ua_event ev,
 		if (account_sip_autoredirect(ua_account(ua))) {
 			info("menu: redirecting call to %s\n", prm + 4);
 			ua_connect(ua, NULL, NULL, prm + 4, VIDMODE_ON);
-		} else {
+		}
+		else {
 			info("menu: got call redirect response to %s\n",
 			     prm + 4);
 		}
