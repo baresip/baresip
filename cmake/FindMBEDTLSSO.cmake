d6a13912f82091c3a8dc84d224a48965e499b708
Christian Spielberger <c.spielberger@commend.com>
2023-07-11 09:03:58 +0200

    cmake: simplify find mbedtls shared library

--- cmake/FindMBEDTLSSO.cmake
+++ cmake/FindMBEDTLSSO.cmake
@@ -1,12 +1,3 @@
-find_path(MBEDTLSSO_INCLUDE_DIR
-    NAMES mbedtls/ssl.h mbedtls/md.h mbedtls/md5.h mbedtls/error.h
-          mbedtls/sha1.h mbedtls/sha256.h
-    HINTS
-      "${MBEDTLSSO_INCLUDE_DIRS}"
-      "${MBEDTLSSO_HINTS}/include"
-    PATHS /usr/local/include /usr/include
-)
-
 find_library(MBEDTLSSO_LIBRARY
   NAMES libmbedtls.so libmbedx509.so libmbedcrypto.so
   HINTS
@@ -17,14 +8,12 @@ find_library(MBEDTLSSO_LIBRARY
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MBEDTLSSO DEFAULT_MSG
-    MBEDTLSSO_INCLUDE_DIR MBEDTLSSO_LIBRARY)
+    MBEDTLSSO_LIBRARY)
 
 if(MBEDTLSSO_FOUND)
-  set( MBEDTLSSO_INCLUDE_DIRS ${MBEDTLSSO_INCLUDE_DIR} )
   set( MBEDTLSSO_LIBRARIES ${MBEDTLSSO_LIBRARY} )
 else()
-  set( MBEDTLSSO_INCLUDE_DIRS )
   set( MBEDTLSSO_LIBRARIES )
 endif()
 
-mark_as_advanced(MBEDTLSSO_INCLUDE_DIRS MBEDTLSSO_LIBRARIES)
+mark_as_advanced(MBEDTLSSO_LIBRARIES)
