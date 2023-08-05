b60bd50edd98641652bc5854df5bb9758f044a90
Christian Spielberger <c.spielberger@commend.com>
2023-08-05 14:02:41 +0200

    test: call - print info if rule does not match

--- test/call.c
+++ test/call.c
@@ -216,37 +216,69 @@ static void process_rules(struct agent *ag, enum ua_event ev, const char *prm)
 			continue;
 
 		if (str_isset(rule->prm) &&
-		    str_casecmp(prm, rule->prm))
+		    str_casecmp(prm, rule->prm)) {
+			info("test: event %s prm=%s (expected %s)\n",
+			     uag_event_str(ev), prm, rule->prm);
 			continue;
+		}
 
 		if (rule->ua &&
-		    ag->ua != rule->ua)
+		    ag->ua != rule->ua) {
+			info("test: event %s ua=[%s] (expected [%s]\n",
+			     uag_event_str(ev),
+			     account_aor(ua_account(ag->ua)),
+			     account_aor(ua_account(rule->ua)));
 			continue;
+		}
 
 		if (rule->n_incoming &&
-		    ag->n_incoming != rule->n_incoming)
+		    ag->n_incoming != rule->n_incoming) {
+			info("test: event %s n_incoming=%u (expected %u)\n",
+			     uag_event_str(ev),
+			     ag->n_incoming, rule->n_incoming);
 			continue;
+		}
 
 		if (rule->n_progress &&
-		    ag->n_progress != rule->n_progress)
+		    ag->n_progress != rule->n_progress) {
+			info("test: event %s n_progress=%u (expected %u)\n",
+			     uag_event_str(ev),
+			     ag->n_progress, rule->n_progress);
 			continue;
+		}
 
 		if (rule->n_established &&
-		    ag->n_established != rule->n_established)
+		    ag->n_established != rule->n_established) {
+			info("test: event %s n_established=%u (expected %u)\n",
+			     uag_event_str(ev),
+			     ag->n_established, rule->n_established);
 			continue;
+		}
 
 		if (rule->n_audio_estab &&
-		    ag->n_audio_estab != rule->n_audio_estab)
+		    ag->n_audio_estab != rule->n_audio_estab) {
+			info("test: event %s n_audio_estab=%u (expected %u)\n",
+			     uag_event_str(ev),
+			     ag->n_audio_estab, rule->n_audio_estab);
 			continue;
+		}
 
 		if (rule->n_video_estab &&
-		    ag->n_video_estab != rule->n_video_estab)
+		    ag->n_video_estab != rule->n_video_estab) {
+			info("test: event %s n_video_estab=%u (expected %u)\n",
+			     uag_event_str(ev),
+			     ag->n_video_estab, rule->n_video_estab);
 			continue;
+		}
 
 		++f->n_rules_ok;
 		if (rule->n_rules_ok &&
-		    rule->n_rules_ok < f->n_rules_ok)
+		    rule->n_rules_ok < f->n_rules_ok) {
+			info("test: event %s n_rules_ok=%u (expected %u)\n",
+			     uag_event_str(ev),
+			     f->n_rules_ok, rule->n_rules_ok);
 			continue;
+		}
 
 		re_cancel();
 	}
