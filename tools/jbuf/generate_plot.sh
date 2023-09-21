696eede13c9a6664fce8a80939a4096bf3ca6a96
Christian Spielberger <c.spielberger@commend.com>
2023-09-21 06:46:27 +0200

    tools: add jbuf audio call plots

--- tools/jbuf/generate_plot.sh
+++ tools/jbuf/generate_plot.sh
@@ -29,44 +29,50 @@ function gen_datfile() {
 source ./jitter.sh
 init_jitter $netif
 
-i=1
+strm="audio"
 for jmin in 0 10 20; do
-    echo "########### jitter buffer $jmin ###############"
+    for i in 0 1; do
+        if [ "$i" == "0" ]; then
+            strm="audio"
+        else
+            strm="video"
+        fi
 
-    sed -e "s/video_jitter_buffer_delay\s*[0-9]*\-.*/video_jitter_buffer_delay   $jmin-500/" -i video/config
-    baresip -v -f video > /tmp/b.log 2>&1 &
-    sleep 1
-    echo "/dial $target" | nc -N localhost 5555
+        echo "########### jitter buffer $strm $jmin ###############"
 
-    sleep 3
+        sed -e "s/${strm}_jitter_buffer_delay\s*[0-9]*\-.*/${strm}_jitter_buffer_delay   ${jmin}-500/" -i ${strm}/config
+        baresip -v -f ${strm} > /tmp/${strm}.log 2>&1 &
+        sleep 1
+        echo "/dial $target" | nc -N localhost 5555
 
-    enable_jitter
+        sleep 3
 
-    sleep 5
+        enable_jitter
 
-    disable_jitter
+        sleep 5
 
-    sleep 25
+        disable_jitter
 
-    echo "/quit" | nc -N localhost 5555
+        sleep 25
 
-    sleep 1
+        echo "/quit" | nc -N localhost 5555
 
-    gen_datfile "P" jbuf.dat
-    gen_datfile "O" overrun.dat
-    gen_datfile "U" underflow.dat
-    gen_datfile "L" toolate.dat
-    gen_datfile "D" duplicate.dat
-    gen_datfile "S" oosequence.dat
-    gen_datfile "T" lost.dat
-#    cat jbuf.json | jq -r '.traceEvents[] | select (.ph == "P") | .args.line' > jbuf.dat
+        sleep 1
 
-    ./jbuf.plot
-    if [ ! -d plots ]; then
-        mkdir plots
-    fi
-    cp jbuf.eps plots/jbuf_${jmin}.eps
-    i=$(( i+1 ))
+        gen_datfile "P" jbuf.dat
+        gen_datfile "O" overrun.dat
+        gen_datfile "U" underflow.dat
+        gen_datfile "L" toolate.dat
+        gen_datfile "D" duplicate.dat
+        gen_datfile "S" oosequence.dat
+        gen_datfile "T" lost.dat
+
+        ./jbuf.plot
+        if [ ! -d plots ]; then
+            mkdir plots
+        fi
+        cp jbuf.eps plots/jbuf_${strm}_${jmin}.eps
+    done
 
     if [ $once == "true" ]; then
         exit 0
