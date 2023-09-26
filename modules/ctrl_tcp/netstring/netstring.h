70ac2b5c59c00c38bced00cab6fbd1b91c99e8de
Sebastian Reimers <hallo@studio-link.de>
2023-09-26 11:17:39 +0200

    ctrl_tcp: fix netstring enum warning (#2730)

--- modules/ctrl_tcp/netstring/netstring.h
+++ modules/ctrl_tcp/netstring/netstring.h
@@ -10,7 +10,17 @@
 
 #include <string.h>
 
-const char* netstring_error_str(int err);
+/* Errors that can occur during netstring parsing */
+typedef enum {
+	NETSTRING_ERROR_TOO_LONG = -100,
+	NETSTRING_ERROR_NO_COLON,
+	NETSTRING_ERROR_TOO_SHORT,
+	NETSTRING_ERROR_NO_COMMA,
+	NETSTRING_ERROR_LEADING_ZERO,
+	NETSTRING_ERROR_NO_LENGTH
+} netstring_error;
+
+const char* netstring_error_str(netstring_error err);
 
 int netstring_read(char *buffer, size_t buffer_length,
 		   char **netstring_start, size_t *netstring_length);
@@ -21,15 +31,5 @@ size_t netstring_buffer_size(size_t data_length);
 size_t netstring_encode_new(char **netstring, char *data, size_t len);
 
 #define NETSTRING_MAX_SIZE 999999999
-
-/* Errors that can occur during netstring parsing */
-typedef enum {
-	NETSTRING_ERROR_TOO_LONG = -100,
-	NETSTRING_ERROR_NO_COLON,
-	NETSTRING_ERROR_TOO_SHORT,
-	NETSTRING_ERROR_NO_COMMA,
-	NETSTRING_ERROR_LEADING_ZERO,
-	NETSTRING_ERROR_NO_LENGTH
-} netstring_error;
 
 #endif
