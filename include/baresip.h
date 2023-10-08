87bdfc950e6bda62d63e2210a416191296c75516
Sebastian Reimers <hallo@studio-link.de>
2023-10-08 16:49:02 +0200

    jbuf: use clock based playout time

--- include/baresip.h
+++ include/baresip.h
@@ -1541,6 +1541,7 @@ struct jbuf_stat {
 
 
 int  jbuf_alloc(struct jbuf **jbp, uint32_t min, uint32_t max);
+void jbuf_set_srate(struct jbuf *jb, uint32_t srate);
 int  jbuf_set_type(struct jbuf *jb, enum jbuf_type jbtype);
 int  jbuf_put(struct jbuf *jb, const struct rtp_header *hdr, void *mem);
 int  jbuf_get(struct jbuf *jb, struct rtp_header *hdr, void **mem);
