0acead3538ec5cf799e169df7cf54f8fe89434a8
Sebastian Reimers <hallo@studio-link.de>
2023-10-12 17:34:12 +0200

    add tracing and experimental tmr optimizations

--- include/baresip.h
+++ include/baresip.h
@@ -1570,6 +1570,7 @@ struct jbuf_stat {
 
 int  jbuf_alloc(struct jbuf **jbp, uint32_t min, uint32_t max);
 void jbuf_set_srate(struct jbuf *jb, uint32_t srate);
+void jbuf_set_id(struct jbuf *jb, struct pl *id);
 int  jbuf_set_type(struct jbuf *jb, enum jbuf_type jbtype);
 void jbuf_set_gnack(struct jbuf *jb, struct rtp_sock *rtp);
 int  jbuf_put(struct jbuf *jb, const struct rtp_header *hdr, void *mem);
