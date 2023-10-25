117b01b25cd8f30bd7baffaf9ab5ffff587e7b97
Christian Spielberger <c.spielberger@commend.com>
2023-10-25 16:29:36 +0200

    test: call - less logging in auframe handler

--- test/call.c
+++ test/call.c
@@ -1648,8 +1648,8 @@ static void auframe_handler(struct auframe *af, void *arg)
 	ua = ag->ua;
 	/* Does auframe come from the decoder ? */
 	if (!audio_rxaubuf_started(call_audio(ua_call(ua)))) {
-		info("test: [%s] no audio received from decoder yet\n",
-		     account_aor(ua_account(ua)));
+		debug("test: [%s] no audio received from decoder yet\n",
+		      account_aor(ua_account(ua)));
 		return;
 	}
 
