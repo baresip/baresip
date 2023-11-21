231b83d8ca893e0b0c3e47531495eee8c5b76f51
Christian Spielberger <c.spielberger@commend.com>
2023-11-21 13:33:33 +0100

    rtprecv: increase RX thread priority

--- src/rtprecv.c
+++ src/rtprecv.c
@@ -8,6 +8,8 @@
 #include <re_atomic.h>
 #include <re.h>
 #include <baresip.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #include "core.h"
 
 /** Magic number */
@@ -218,6 +220,10 @@ static int rtprecv_thread(void *arg)
 			err);
 		return err;
 	}
+
+	int n = setpriority(PRIO_PROCESS, 0, -10);
+	if (n == -1)
+		warning("rtp_receiver: could not set nice value (%m)\n", errno);
 
 	err = re_main(NULL);
 
