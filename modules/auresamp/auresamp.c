d277e0ebc69133e69c1c77de4f4d301f2da0735f
Christian Spielberger <c.spielberger@commend.com>
2024-05-29 09:26:31 +0200

    auresamp: fix size of sampv if aufmt conversion needed

--- modules/auresamp/auresamp.c
+++ modules/auresamp/auresamp.c
@@ -71,10 +71,17 @@ static void dec_destructor(void *arg)
 
 static int sampv_alloc(struct auresamp_st *st, struct auframe *af)
 {
+	size_t psize_out;
 	size_t psize;
 
-	psize = auframe_size(af);
-	st->sampv = mem_zalloc(psize, NULL);
+	/* s16le used internally */
+	psize = af->sampc * af->ch * 2;
+
+	/* output format == input format */
+	psize_out = aufmt_sample_size(af->fmt) * af->sampc *
+			st->oprm.srate * st->oprm.ch / (af->srate * af->ch);
+
+	st->sampv = mem_zalloc(max(psize, psize_out), NULL);
 
 	if (!st->sampv)
 		return ENOMEM;
