ec4a0e8048d34b4b4c427795b657f1d9333235c4
Christian Spielberger <c.spielberger@commend.com>
2024-06-05 10:30:53 +0200

    Revert "test: add aufile duration test"
    This reverts commit a1bf0b82c358d3a754f0a11e20f51c8d8d8488a0.

--- test/main.c
+++ test/main.c
@@ -24,7 +24,6 @@ struct test {
 static const struct test tests[] = {
 	TEST(test_account),
 	TEST(test_account_uri_complete),
-	TEST(test_aufile_duration),
 	TEST(test_call_answer),
 	TEST(test_call_answer_hangup_a),
 	TEST(test_call_answer_hangup_b),
