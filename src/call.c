36a8350396692fb93fcb7902cf6bd462bc838ea5
Juha Heinanen <jh@tutpro.com>
2024-07-26 12:43:52 +0300

    Do not send 100 Tryiung from sipsess_accept

--- src/call.c
+++ src/call.c
@@ -2233,7 +2233,7 @@ int call_accept(struct call *call, struct sipsess_sock *sess_sock,
 		mem_deref(rid);
 	}
 
-	err = sipsess_accept(&call->sess, sess_sock, msg, 100, "Trying",
+	err = sipsess_accept(&call->sess, sess_sock, msg, 0, "",
 			     account_rel100_mode(call->acc),
 			     ua_cuser(call->ua), "application/sdp", NULL,
 			     auth_handler, call->acc, true,
