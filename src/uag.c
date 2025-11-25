9c9e48772198a41052fdc7c218b9632a6a6dbb83
Christian Spielberger <c.spielberger@commend.com>
2025-11-25 11:24:08 +0100

    uag: use GRUU if available for selecting UA
    Due to RFC-5627 Section 4.1 a registrar MUST generate a GRUU if Contact
    parameter ;pub-gruu appears in the SIP REGISTER and use it to route following
    SIP requets to the unique UA. Thus baresip needs to compare the gruu if it is
    set to select the right UA.

--- src/uag.c
+++ src/uag.c
@@ -941,7 +941,7 @@ struct ua *uag_find_msg(const struct sip_msg *msg)
 		if (!acc || !acc->regint)
 			continue;
 
-		if (0 == pl_strcasecmp(cuser, ua_local_cuser(ua))) {
+		if (0 == pl_strcasecmp(cuser, ua_cuser(ua))) {
 			ua_printf(ua, "selected for %r\n", cuser);
 			return ua;
 		}
