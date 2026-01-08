8e1fb6a08f46cf0d742bf0fa10ff622c7009031b
Christian Spielberger <c.spielberger@commend.com>
2026-01-08 08:36:01 +0100

    test: split test/call.c (#3610)
    * test: split test/call.c
    
    * test: call - pr comments, cleanup

---
+++ test/call.h
@@ -0,0 +1,170 @@
+/**
+ * @file call.h  Call tests -- internal API
+ *
+ * Copyright (C) 2026 Alfred E. Heggestad, Christian Spielberger
+ */
+
+
+#define MAGIC 0x7004ca11
+
+enum {
+	IP_127_0_0_1 = 0x7f000001,
+};
+
+enum behaviour {
+	BEHAVIOUR_ANSWER = 0,
+	BEHAVIOUR_NOTHING,
+	BEHAVIOUR_REJECT,
+	BEHAVIOUR_REJECTF,
+	BEHAVIOUR_GET_HDRS,
+	BEHAVIOUR_PROGRESS,
+};
+
+enum action {
+	ACTION_RECANCEL = 0,
+	ACTION_HANGUP_A,
+	ACTION_HANGUP_B,
+	ACTION_NOTHING,
+	ACTION_TRANSFER,
+	ACTION_ATT_TRANSFER,
+};
+
+
+struct cancel_rule {
+	struct le le;
+
+	enum bevent_ev ev;
+	const char *prm;
+	struct ua *ua;
+	bool checkack;
+
+	unsigned n_incoming;
+	unsigned n_progress;
+	unsigned n_established;
+	unsigned n_audio_estab;
+	unsigned n_video_estab;
+	unsigned n_offer_cnt;
+	unsigned n_answer_cnt;
+	unsigned n_vidframe;
+	unsigned n_auframe;
+	unsigned n_audebug;
+	unsigned n_rtcp;
+	unsigned n_closed;
+	double aulvl;
+
+	struct cancel_rule *cr_and;
+	bool met;
+};
+
+struct agent {
+	struct fixture *fix;    /* pointer to parent */
+	struct agent *peer;
+	struct ua *ua;
+	uint16_t close_scode;
+	char *close_prm;
+	bool failed;
+
+	unsigned n_incoming;
+	unsigned n_progress;
+	unsigned n_established;
+	unsigned n_closed;
+	unsigned n_transfer_fail;
+	unsigned n_dtmf_recv;
+	unsigned n_transfer;
+	unsigned n_mediaenc;
+	unsigned n_rtpestab;
+	unsigned n_rtcp;
+	unsigned n_audio_estab;
+	unsigned n_video_estab;
+	unsigned n_offer_cnt;
+	unsigned n_answer_cnt;
+	unsigned n_hold_cnt;
+	unsigned n_resume_cnt;
+	unsigned n_vidframe;
+	unsigned n_auframe;
+	unsigned n_audebug;
+	double aulvl;
+
+	struct tmr tmr_ack;
+	bool gotack;
+
+	struct tmr tmr;
+};
+
+
+struct fixture {
+	uint32_t magic;
+	struct agent a, b, c;
+	struct sa dst;
+	struct sa laddr_udp;
+	struct sa laddr_tcp;
+	struct sa laddr_tls;
+	enum behaviour behaviour;
+	enum action estab_action;
+	char buri[256];
+	char buri_tcp[256];
+	int err;
+	struct call *xfer;
+	unsigned exp_estab;
+	unsigned exp_closed;
+	bool fail_transfer;
+	const struct list *hdrs;
+	const char *dtmf_digits;
+	struct list rules;
+};
+
+
+int fixture_init_priv(struct fixture *f, const char *prm);
+void fixture_close(struct fixture *f);
+void fixture_abort(struct fixture *f, int err);
+
+
+#define fixture_init_prm(f, prm) \
+	err = fixture_init_priv((f), (prm)); \
+	TEST_ERR(err);
+
+#define fixture_init(f) \
+	err = fixture_init_priv((f), ("")); \
+	TEST_ERR(err);
+
+void process_rules(struct agent *ag, enum bevent_ev ev, const char *prm);
+
+
+struct cancel_rule *fixture_add_cancel_rule(struct fixture *f,
+					    enum bevent_ev ev,
+					    struct ua *ua,
+					    unsigned n_incoming,
+					    unsigned n_progress,
+					    unsigned n_established);
+struct cancel_rule *cancel_rule_and_alloc(struct cancel_rule *cr,
+					  enum bevent_ev ev,
+					  struct ua *ua,
+					  unsigned n_incoming,
+					  unsigned n_progress,
+					  unsigned n_established);
+void failure_debug(struct fixture *f, bool c);
+void check_ack(void *arg);
+int agent_wait_for_ack(struct agent *ag, unsigned n_incoming,
+		       unsigned n_progress, unsigned n_established);
+
+#define cancel_rule_new(ev, ua, n_incoming, n_progress, n_established)    \
+	cr = fixture_add_cancel_rule(f, ev, ua, n_incoming, n_progress,   \
+				     n_established);			  \
+	if (!cr) {							  \
+		err = ENOMEM;						  \
+		goto out;						  \
+	}
+
+
+#define cancel_rule_and(ev, ua, n_incoming, n_progress, n_established)	  \
+	cr = cancel_rule_and_alloc(cr, ev, ua, n_incoming, n_progress,	  \
+				   n_established);			  \
+	if (!cr) {							  \
+		err = ENOMEM;						  \
+		goto out;						  \
+	}
+
+
+#define cancel_rule_pop()						  \
+	mem_deref(list_tail(&f->rules)->data);
+
---
+++ test/call_fixture.c
@@ -0,0 +1,426 @@
+/**
+ * @file call_fixture.c  Call tests -- Test fixture and other helpers
+ *
+ * Copyright (C) 2026 Alfred E. Heggestad, Christian Spielberger
+ */
+
+
+#include <string.h>
+#include <re.h>
+#include <baresip.h>
+#include "test.h"
+#include "call.h"
+
+#define DEBUG_MODULE "testcall"
+#define DEBUG_LEVEL 5
+#include <re_dbg.h>
+
+static void event_handler(enum bevent_ev ev, struct bevent *event, void *arg)
+{
+	struct fixture *f = arg;
+	struct call *call2 = NULL;
+	struct agent *ag;
+	struct stream *strm = NULL;
+	char curi[256];
+	const char    *prm  = bevent_get_text(event);
+	struct call   *call = bevent_get_call(event);
+	struct ua     *ua   = bevent_get_ua(event);
+	const struct sip_msg *msg  = bevent_get_msg(event);
+	int err = 0;
+
+#if 1
+	info("test: [ %s ] event: %s (%s)\n",
+	     account_aor(ua_account(ua)), bevent_str(ev), prm);
+#endif
+
+	ASSERT_TRUE(f != NULL);
+	ASSERT_EQ(MAGIC, f->magic);
+
+	if (ev == BEVENT_CREATE)
+		return;
+
+	if (!ua)
+		ua = uag_find_msg(msg);
+
+	if (ua && ev == BEVENT_SIPSESS_CONN) {
+		err = ua_accept(ua, msg);
+		if (err) {
+			warning("test: could not accept incoming call (%m)\n",
+				err);
+			return;
+		}
+
+		bevent_stop(event);
+	}
+
+	if (ua == f->a.ua)
+		ag = &f->a;
+	else if (ua == f->b.ua)
+		ag = &f->b;
+	else if (ua == f->c.ua)
+		ag = &f->c;
+	else {
+		warning("test: could not find agent/ua\n");
+		return;
+	}
+
+	switch (ev) {
+
+	case BEVENT_CALL_REDIRECT:
+		ASSERT_STREQ("302,sip:c@127.0.0.1", prm);
+		break;
+
+	case BEVENT_CALL_INCOMING:
+		++ag->n_incoming;
+
+		switch (f->behaviour) {
+
+		case BEHAVIOUR_ANSWER:
+			err = ua_answer(ua, call, VIDMODE_ON);
+			if (err) {
+				warning("ua_answer failed (%m)\n", err);
+				goto out;
+			}
+			break;
+
+		case BEHAVIOUR_REJECT:
+			ua_hangup(ua, call, 0, 0);
+			call = NULL;
+			ag->failed = true;
+			break;
+
+		case BEHAVIOUR_REJECTF:
+			ua_hangupf(ua, call, 302, "Moved Temporarily",
+				"Contact: <sip:c@127.0.0.1>;expires=5\r\n"
+				"Diversion: <sip:a@127.0.0.1>;reason=nop\r\n"
+				"Content-Length: 0\r\n\r\n");
+			call = NULL;
+			ag->failed = true;
+			break;
+
+		case BEHAVIOUR_GET_HDRS:
+			f->hdrs = call_get_custom_hdrs(call);
+			err = ua_answer(ua, call, VIDMODE_ON);
+			if (err) {
+				warning("ua_answer failed (%m)\n", err);
+				goto out;
+			}
+			break;
+
+		case BEHAVIOUR_PROGRESS:
+			err = call_progress(call);
+			if (err) {
+				warning("call_progress failed (%m)\n", err);
+				goto out;
+			}
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case BEVENT_CALL_PROGRESS:
+		++ag->n_progress;
+
+		break;
+
+	case BEVENT_CALL_ESTABLISHED:
+		++ag->n_established;
+
+		ASSERT_TRUE(str_isset(call_id(call)));
+
+		/* are both agents established? */
+		if (ag->n_established >= f->exp_estab &&
+		    ag->peer->n_established >= f->exp_estab) {
+
+			switch (f->estab_action) {
+
+			case ACTION_RECANCEL:
+				re_cancel();
+				break;
+
+			case ACTION_HANGUP_A:
+				f->a.failed = true;
+				ua_hangup(f->a.ua, NULL, 0, 0);
+				break;
+
+			case ACTION_HANGUP_B:
+				f->b.failed = true;
+				ua_hangup(f->b.ua, NULL, 0, 0);
+				break;
+
+			case ACTION_NOTHING:
+				/* Do nothing, wait */
+				break;
+
+			case ACTION_TRANSFER:
+				f->estab_action = ACTION_NOTHING;
+
+				if (f->fail_transfer)
+					f->behaviour = BEHAVIOUR_REJECT;
+
+				re_snprintf(curi, sizeof(curi),
+					    "sip:c@%J", &f->laddr_udp);
+
+				err = call_hold(ua_call(f->a.ua), true);
+				if (err)
+					goto out;
+
+				err = call_transfer(ua_call(f->a.ua), curi);
+				if (err)
+					goto out;
+				break;
+
+			case ACTION_ATT_TRANSFER:
+				re_snprintf(curi, sizeof(curi), "sip:c@%J",
+					&f->laddr_udp);
+
+				if (f->xfer) {
+					err = call_hold(ua_call(ua), true);
+					if (err)
+						goto out;
+
+					err = call_replace_transfer(f->xfer,
+						ua_call(ua));
+					if (err)
+						goto out;
+
+					f->xfer = NULL;
+					f->estab_action = ACTION_NOTHING;
+					break;
+				}
+
+				err = call_hold(ua_call(ua), true);
+				if (err)
+					goto out;
+
+				err = ua_connect(ua, NULL, NULL,
+					curi, VIDMODE_ON);
+				if (err)
+					goto out;
+
+				f->xfer = call;
+
+				break;
+			}
+		}
+		break;
+
+	case BEVENT_CALL_CLOSED:
+		++ag->n_closed;
+
+		ag->close_scode = call_scode(call);
+		mem_deref(ag->close_prm);
+		str_dup(&ag->close_prm, prm);
+
+		if (ag->close_scode)
+			ag->failed = true;
+
+		if (ag->n_closed >= f->exp_closed &&
+		    ag->peer->n_closed >= f->exp_closed) {
+
+			re_cancel();
+		}
+		break;
+
+	case BEVENT_CALL_TRANSFER:
+		++ag->n_transfer;
+
+		err = ua_call_alloc(&call2, ua, VIDMODE_ON, NULL, call,
+				call_localuri(call), true);
+		if (!err) {
+			call_set_user_data(call2, call_user_data(call));
+
+			struct pl pl;
+			pl_set_str(&pl, prm);
+			err = call_connect(call2, &pl);
+			if (err) {
+				warning("ua: transfer: connect error: %m\n",
+					err);
+			}
+		}
+
+		if (err) {
+			(void)call_notify_sipfrag(call, 500, "Call Error");
+			mem_deref(call2);
+		}
+		break;
+
+	case BEVENT_CALL_TRANSFER_FAILED:
+		++ag->n_transfer_fail;
+
+		call_hold(call, false);
+		if (ua == f->a.ua)
+			re_cancel();
+
+		break;
+
+	case BEVENT_CALL_REMOTE_SDP:
+		if (!str_cmp(prm, "offer"))
+			++ag->n_offer_cnt;
+		else if (!str_cmp(prm, "answer"))
+			++ag->n_answer_cnt;
+
+		break;
+
+	case BEVENT_CALL_HOLD:
+		++ag->n_hold_cnt;
+		break;
+
+	case BEVENT_CALL_RESUME:
+		++ag->n_resume_cnt;
+		break;
+
+	case BEVENT_CALL_MENC:
+		++ag->n_mediaenc;
+
+		if (strstr(prm, "audio"))
+			strm = audio_strm(call_audio(call));
+		else if (strstr(prm, "video"))
+			strm = video_strm(call_video(call));
+
+		if (strm) {
+			ASSERT_TRUE(stream_is_secure(strm));
+		}
+		break;
+
+	case BEVENT_CALL_DTMF_START:
+		ASSERT_EQ(1, str_len(prm));
+		ASSERT_EQ(f->dtmf_digits[ag->n_dtmf_recv], prm[0]);
+		++ag->n_dtmf_recv;
+
+		if (ag->n_dtmf_recv >= str_len(f->dtmf_digits)) {
+			re_cancel();
+		}
+		break;
+
+	case BEVENT_CALL_RTPESTAB:
+		++ag->n_rtpestab;
+
+		if (strstr(prm, "audio"))
+			++ag->n_audio_estab;
+		else if (strstr(prm, "video"))
+			++ag->n_video_estab;
+
+		break;
+
+	case BEVENT_CALL_RTCP:
+		++ag->n_rtcp;
+
+		break;
+
+	default:
+		break;
+	}
+
+	if (ag->failed && ag->peer->failed) {
+		info("test: re_cancel on call failed\n");
+		re_cancel();
+		return;
+	}
+
+	process_rules(ag, ev, prm);
+
+ out:
+	if (err) {
+		warning("error in event-handler (%m)\n", err);
+		fixture_abort(f, err);
+	}
+}
+
+
+int fixture_init_priv(struct fixture *f, const char *prm)
+{
+	int err = 0;
+	char *buf = NULL;
+	memset(f, 0, sizeof(*f));
+
+	f->a.fix = f;
+	f->b.fix = f;
+	f->c.fix = f;
+	err = sa_set_str(&f->dst, "127.0.0.1", 5060);
+	TEST_ERR(err);
+
+	err = ua_init("test", true, true, true);
+	TEST_ERR(err);
+
+	f->magic = MAGIC;
+	f->estab_action = ACTION_RECANCEL;
+	f->exp_estab = 1;
+	f->exp_closed = 1;
+	/* NOTE: See Makefile TEST_MODULES */
+	err = module_load(".", "g711");
+	TEST_ERR(err);
+
+	err = re_sdprintf(&buf, "A <sip:a@127.0.0.1>;regint=0%s", prm);
+	TEST_ERR(err);
+	err = ua_alloc(&f->a.ua, buf);
+	TEST_ERR(err);
+	buf = mem_deref(buf);
+	err = re_sdprintf(&buf, "B <sip:b@127.0.0.1>;regint=0%s", prm);
+	TEST_ERR(err);
+	err = ua_alloc(&f->b.ua, buf);
+	TEST_ERR(err);
+
+	f->a.peer = &f->b;
+	f->b.peer = &f->a;
+
+	err = bevent_register(event_handler, f);
+	TEST_ERR(err);
+
+	err = sip_transp_laddr(uag_sip(), &f->laddr_udp,
+			       SIP_TRANSP_UDP, &f->dst);
+	TEST_ERR(err);
+
+	err = sip_transp_laddr(uag_sip(), &f->laddr_tcp,
+			       SIP_TRANSP_TCP, &f->dst);
+	TEST_ERR(err);
+
+	err = sip_transp_laddr(uag_sip(), &f->laddr_tls,
+			       SIP_TRANSP_TLS, &f->dst);
+	TEST_ERR(err);
+
+	debug("test: local SIP transp: UDP=%J, TCP=%J\n",
+	      &f->laddr_udp, &f->laddr_tcp);
+
+	re_snprintf(f->buri, sizeof(f->buri),
+		    "sip:b@%J", &f->laddr_udp);
+	re_snprintf(f->buri_tcp, sizeof(f->buri_tcp),
+		    "sip:b@%J;transport=tcp", &f->laddr_tcp);
+out:
+	mem_deref(buf);
+	return err;
+}
+
+
+void fixture_close(struct fixture *f)
+{
+	tmr_cancel(&f->a.tmr_ack);
+	tmr_cancel(&f->b.tmr_ack);
+	tmr_cancel(&f->c.tmr_ack);
+	tmr_cancel(&f->a.tmr);
+	tmr_cancel(&f->b.tmr);
+	tmr_cancel(&f->c.tmr);
+	mem_deref(f->c.ua);
+	mem_deref(f->b.ua);
+	mem_deref(f->a.ua);
+	mem_deref(f->c.close_prm);
+	mem_deref(f->b.close_prm);
+	mem_deref(f->a.close_prm);
+
+	module_unload("g711");
+
+	bevent_unregister(event_handler);
+
+	ua_stop_all(true);
+	ua_close();
+	list_flush(&f->rules);
+}
+
+
+void fixture_abort(struct fixture *f, int err)
+{
+	f->err = err;
+	re_cancel();
+}
