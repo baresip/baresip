23af77aec4625635f92a702ed0eafd622d93fc2f
Christian Spielberger <c.spielberger@commend.com>
2026-01-12 14:50:17 +0100

    test: common auframe handler for auplay

--- test/test.h
+++ test/test.h
@@ -154,6 +154,7 @@ int dns_server_add_srv(struct dns_server *srv, const char *name,
  */
 
 struct auplay;
+struct ausrc;
 
 typedef void (mock_sample_h)(struct auframe *af, const char *dev, void *arg);
 
