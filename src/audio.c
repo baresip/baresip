d0cbfc66818e7f4d9889f8551c27e6461b5df005
Alfred E. Heggestad <114750+alfredh@users.noreply.github.com>
2026-08-07 15:10:45 +0200

    audio: fix warning() %d format in encode_rtp_send()

--- src/audio.c
+++ src/audio.c
@@ -355,7 +355,7 @@ static void encode_rtp_send(struct audio *a, struct autx *tx,
 		af->sampc = 0;
 	}
 	else if (err) {
-		warning("audio: %s encode error: %d samples (%m)\n",
+		warning("audio: %s encode error: %zu samples (%m)\n",
 			tx->ac->name, af->sampc, err);
 		goto out;
 	}
